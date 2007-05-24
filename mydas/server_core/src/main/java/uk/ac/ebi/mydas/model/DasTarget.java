789b4855105a9bd031fc602a11939cfe3c72b9b0
philip.j.r.jones <philip.j.r.jones@8fdf904b-3330-0410-8a44-cd576ecb14f1>
2007-05-24 16:47:55 +0000

    Documentation added to fields and the constructor.
    git-svn-id: http://mydas.googlecode.com/svn/trunk@126 8fdf904b-3330-0410-8a44-cd576ecb14f1

--- mydas/server_core/src/main/java/uk/ac/ebi/mydas/model/DasTarget.java
+++ mydas/server_core/src/main/java/uk/ac/ebi/mydas/model/DasTarget.java
@@ -23,6 +23,8 @@
 
 package uk.ac.ebi.mydas.model;
 
+import uk.ac.ebi.mydas.exceptions.DataSourceException;
+
 /**
  * Created Using IntelliJ IDEA.
  * Date: 14-May-2007
@@ -32,22 +34,67 @@ package uk.ac.ebi.mydas.model;
  */
 public class DasTarget {
 
+    /**
+     * <b>Mandatory</b> field, corresponds to
+     * the following attribute in the response to the
+     * feature command:
+     *
+     * /DASGFF/GFF/SEGMENT/FEATURE/TARGET/@id
+     */
     private String targetId;
 
+    /**
+     * <b>Optional</b> field, corresponds to
+     * the value of the following element in the response to the
+     * feature command:
+     *
+     * /DASGFF/GFF/SEGMENT/FEATURE/TARGET
+     */
     private String targetName;
 
+    /**
+     * <b>Mandatory</b> field, corresponds to
+     * the following attribute in the response to the
+     * feature command:
+     *
+     * /DASGFF/GFF/SEGMENT/FEATURE/TARGET/@start
+     */
     private int startCoordinate;
 
+    /**
+     * <b>Mandatory</b> field, corresponds to
+     * the following attribute in the response to the
+     * feature command:
+     *
+     * /DASGFF/GFF/SEGMENT/FEATURE/TARGET/@stop
+     */
     private int stopCoordinate;
 
     /**
      * Constructor for a DasTarget, serialized out as the response to a feature request.
-     * @param targetId <b>Mandatory</b>
-     * @param startCoordinate <b>Mandatory</b>
-     * @param stopCoordinate <b>Mandatory</b>
-     * @param targetName <b>Optional</b>
+     * @param targetId <b>Mandatory</b> field, corresponds to
+     * the following attribute in the response to the
+     * feature command:
+     * /DASGFF/GFF/SEGMENT/FEATURE/TARGET/@id
+     * @param startCoordinate <b>Mandatory</b> field, corresponds to
+     * the following attribute in the response to the
+     * feature command:
+     * /DASGFF/GFF/SEGMENT/FEATURE/TARGET/@start
+     * @param stopCoordinate <b>Mandatory</b> field, corresponds to
+     * the following attribute in the response to the
+     * feature command:
+     * /DASGFF/GFF/SEGMENT/FEATURE/TARGET/@stop
+     * @param targetName <b>Optional</b> field, corresponds to
+     * the value of the following element in the response to the
+     * feature command:
+     * /DASGFF/GFF/SEGMENT/FEATURE/TARGET
+     * @throws uk.ac.ebi.mydas.exceptions.DataSourceException
+     * if the targetId attribute has not been set to a valid value (i.e. non-zero length String).
      */
-    public DasTarget(String targetId, int startCoordinate, int stopCoordinate, String targetName) {
+    public DasTarget(String targetId, int startCoordinate, int stopCoordinate, String targetName) throws DataSourceException {
+        if (targetId == null || targetId.length() == 0){
+            throw new DataSourceException("The mandatory targetId parameter has been set as null / empty String when attempting to instantiate a DasTarget object.");
+        }
         this.targetId = targetId;
         this.startCoordinate = startCoordinate;
         this.stopCoordinate = stopCoordinate;
