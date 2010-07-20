46f2acf7c543a178ba738ca6d45c74ce7982604b
leylajael@gmail.com <leylajael@gmail.com@8fdf904b-3330-0410-8a44-cd576ecb14f1>
2010-07-20 16:57:41 +0000

    Testing new files to subversion
    git-svn-id: http://mydas.googlecode.com/svn/trunk@380 8fdf904b-3330-0410-8a44-cd576ecb14f1

---
+++ mydas/server_core/src/main/java/uk/ac/ebi/mydas/configuration/DeleteMe.java
@@ -0,0 +1,11 @@
+package uk.ac.ebi.mydas.configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ljgarcia
+ * Date: 20-Jul-2010
+ * Time: 17:57:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class DeleteMe {
+}
