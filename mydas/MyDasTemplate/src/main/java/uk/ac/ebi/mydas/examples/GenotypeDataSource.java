995869e7f4be2cfd9711b6f990e206dea182f161
leylajael@gmail.com <leylajael@gmail.com@8fdf904b-3330-0410-8a44-cd576ecb14f1>
2011-11-08 09:30:00 +0000

    Genotype examples provided by Jonathan Warren (Sanger)
    git-svn-id: http://mydas.googlecode.com/svn/trunk@1032 8fdf904b-3330-0410-8a44-cd576ecb14f1

---
+++ mydas/MyDasTemplate/src/main/java/uk/ac/ebi/mydas/examples/GenotypeDataSource.java
@@ -0,0 +1,161 @@
+package uk.ac.ebi.mydas.examples;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+
+import uk.ac.ebi.mydas.configuration.DataSourceConfiguration;
+import uk.ac.ebi.mydas.configuration.PropertyType;
+import uk.ac.ebi.mydas.controller.CacheManager;
+import uk.ac.ebi.mydas.datasource.RangeHandlingAnnotationDataSource;
+import uk.ac.ebi.mydas.exceptions.BadReferenceObjectException;
+import uk.ac.ebi.mydas.exceptions.CoordinateErrorException;
+import uk.ac.ebi.mydas.exceptions.DataSourceException;
+import uk.ac.ebi.mydas.exceptions.UnimplementedFeatureException;
+import uk.ac.ebi.mydas.extendedmodel.DasUnknownFeatureSegment;
+import uk.ac.ebi.mydas.model.*;
+
+public class GenotypeDataSource implements RangeHandlingAnnotationDataSource{
+	CacheManager cacheManager = null;
+	ServletContext svCon;
+	Map<String, PropertyType> globalParameters;
+	DataSourceConfiguration config;
+	GenotypeManager genotypeManager;
+
+	public void init(ServletContext servletContext,
+			Map<String, PropertyType> globalParameters,
+			DataSourceConfiguration dataSourceConfig)
+			throws DataSourceException {
+		this.svCon = servletContext;
+		this.globalParameters = globalParameters;
+		this.config = dataSourceConfig;
+		   String databaseUrl="";
+		   String databasePass="";
+		   String databaseUser="";
+		if (config.getDataSourceProperties().containsKey("databaseUrl")){
+			   databaseUrl=config.getDataSourceProperties().get("databaseUrl").getValue();
+		   }
+		if (config.getDataSourceProperties().containsKey("databaseUser")){
+			   databaseUser=config.getDataSourceProperties().get("databaseUser").getValue();
+		   }
+		if (config.getDataSourceProperties().containsKey("databasePass")){
+			   databasePass=config.getDataSourceProperties().get("databasePass").getValue();
+		   }
+		if(databaseUrl.equals("") || databaseUrl==null || databaseUser.equals("") || databaseUser==null|| databasePass.equals("") || databasePass==null){
+			throw new DataSourceException("a database url must be set such in the configuration for example: ");
+		}
+		try {
+			System.out.println("connection params="+databaseUrl+" user:"+databaseUser+" pass:"+databasePass);
+			genotypeManager=new GenotypeManager(databaseUrl, databaseUser, databasePass);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void destroy() {
+		genotypeManager.close();
+	}
+
+	public DasAnnotatedSegment getFeatures(String segmentId, int start, int stop, Integer maxbins) 
+			throws BadReferenceObjectException, CoordinateErrorException, DataSourceException {
+		if (maxbins==null)
+			maxbins=-1;
+		return genotypeManager.getSubmodelBySegmentId(segmentId, start, stop,maxbins);
+	}
+	public DasAnnotatedSegment getFeatures(String segmentId, Integer maxbins) 
+			throws BadReferenceObjectException, DataSourceException {
+		if (maxbins==null)
+			maxbins=-1;
+		return genotypeManager.getSubmodelBySegmentId(segmentId, -1, -1,maxbins);
+	}
+
+	
+	private boolean lookInside(DasComponentFeature component,String featureId,Collection<DasAnnotatedSegment> segmentsResponse,DasAnnotatedSegment segment) throws DataSourceException{
+		if (component.hasSubParts()){
+			for (DasComponentFeature subcomponent: component.getReportableSubComponents()){
+				if(subcomponent.getFeatureId().equals(featureId)){
+					segmentsResponse.add(new DasAnnotatedSegment(segment.getSegmentId(),segment.getStartCoordinate(),segment.getStopCoordinate(),segment.getVersion(),segment.getSegmentLabel(),Collections.singleton((DasFeature)subcomponent)));
+					return true;
+				}else
+					if(this.lookInside(subcomponent, featureId, segmentsResponse, segment))
+						return true;
+			}
+		}
+		return false;
+	}
+
+	public Collection<DasType> getTypes() throws DataSourceException {
+		return genotypeManager.getTypes();
+	}
+	public URL getLinkURL(String field, String id)
+			throws UnimplementedFeatureException, DataSourceException {
+		throw new UnimplementedFeatureException("No implemented");
+	}
+
+
+    public String getEntryPointVersion() throws UnimplementedFeatureException, DataSourceException {
+        return genotypeManager.getDatabase();
+    }
+
+   
+
+    public Integer getTotalCountForType(DasType type)
+			throws DataSourceException {
+		return genotypeManager.getTotalCountForType(type.getId());
+	}
+
+	public void registerCacheManager(CacheManager cacheManager) {
+		this.cacheManager = cacheManager;
+	}
+
+	@Override
+	public DasAnnotatedSegment getFeatures(String segmentId, Integer maxbins,
+			Range rows) throws BadReferenceObjectException,
+			DataSourceException, UnimplementedFeatureException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<DasAnnotatedSegment> getFeatures(
+			Collection<String> featureIdCollection, Integer maxbins, Range rows)
+			throws UnimplementedFeatureException, DataSourceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DasAnnotatedSegment getFeatures(String segmentId, int start,
+			int stop, Integer maxbins, Range rows)
+			throws BadReferenceObjectException, CoordinateErrorException,
+			DataSourceException, UnimplementedFeatureException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<DasAnnotatedSegment> getFeatures(
+			Collection<String> featureIdCollection, Integer maxbins)
+			throws UnimplementedFeatureException, DataSourceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<DasEntryPoint> getEntryPoints(Integer start, Integer stop)
+			throws UnimplementedFeatureException, DataSourceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getTotalEntryPoints() throws UnimplementedFeatureException,
+			DataSourceException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
---
+++ mydas/MyDasTemplate/src/main/java/uk/ac/ebi/mydas/examples/GenotypeManager.java
@@ -0,0 +1,193 @@
+package uk.ac.ebi.mydas.examples;
+
+import uk.ac.ebi.mydas.exceptions.BadReferenceObjectException;
+import uk.ac.ebi.mydas.exceptions.DataSourceException;
+import uk.ac.ebi.mydas.model.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class GenotypeManager {
+	/**
+	 * List of the types used in this data source
+	 */
+	private ArrayList<DasType> types;
+	/**
+	 * Types to be used in the data source: chromosome, gene, transcript and
+	 * exon
+	 */
+	private DasType geneType;
+	/**
+	 * As this data source just have one method, it can be defined as a
+	 * parameter to facilitate its use
+	 */
+	private DasMethod method;
+
+	private Connection connection;
+
+	private String database = "genotype1";
+	private Collection<DasEntryPoint> entryPoints = null;
+
+	public GenotypeManager(String databaseUrl, String databaseUser, String databasePass) throws DataSourceException {
+
+		method = new DasMethod("not_recorded", "not_recorded", "ECO:0000037");
+
+		connection = null;
+
+
+		//String url = "jdbc:mysql://localhost:3306/" + database;
+		// String url = "jdbc:mysql://ensembldb.ensembl.org:5306/"+database;
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			connection = DriverManager.getConnection(databaseUrl, databaseUser, databasePass);
+		} catch (InstantiationException e) {
+			throw new DataSourceException("Problems loading the MySql driver",
+					e);
+		} catch (IllegalAccessException e) {
+			throw new DataSourceException("Problems loading the MySql driver",
+					e);
+		} catch (ClassNotFoundException e) {
+			throw new DataSourceException("Problems loading the MySql driver",
+					e);
+		} catch (SQLException e) {
+			throw new DataSourceException(
+					"Problems connecting to the ensembl database", e);
+		}
+		// Initialize types
+		types = new ArrayList<DasType>();
+		try {
+			Statement s = connection.createStatement();
+			s.executeQuery("select distinct bases from genotype;");
+			ResultSet rs = s.getResultSet();
+			while (rs.next()) {
+				String typeId = rs.getString("bases");
+				System.out.println("typeid=" + typeId);
+				types.add(new DasType(typeId, "", "SO:0000694", ""));
+
+			}
+			rs.close();
+			s.close();
+		} catch (SQLException e) {
+			throw new DataSourceException("Problems executing the sql query", e);
+		}
+	}
+
+	public void close() {
+		try {
+			connection.close();
+		} catch (Exception e) { /* ignore close errors */
+		}
+
+	}
+
+
+
+	public DasAnnotatedSegment getSubmodelBySegmentId(String segmentId,
+			int start, int stop) throws DataSourceException,
+			BadReferenceObjectException {
+		return getSubmodelBySegmentId(segmentId, start, stop, -1);
+	}
+
+	public DasAnnotatedSegment getSubmodelBySegmentId(String segmentId,
+			int start, int stop, int maxbins) throws DataSourceException,
+			BadReferenceObjectException {
+	
+		
+		
+		String sql = "select * from genotype where chromosome="+segmentId+" and position > "+start+" and position < "+stop+";";
+		System.out.println(sql);
+		Collection<DasFeature> features = getGenotypeFeatures( sql);
+		DasAnnotatedSegment segment = null;
+		try {
+			segment = new DasAnnotatedSegment(segmentId,new Integer(start),new Integer(stop),"1.0", segmentId, features);
+		} catch (DataSourceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return segment;
+	}
+	
+
+	private Collection<DasFeature> getGenotypeFeatures(
+			String sql) {
+			
+		Collection <DasFeature> features=new ArrayList();
+		try {
+			Statement s = connection.createStatement();
+			s.executeQuery(sql);
+			ResultSet rs = s.getResultSet();
+			while (rs.next()) {
+				String typeId = rs.getString("bases");
+				String id=rs.getString("rs_id");
+				int position=rs.getInt("position");
+				//rs.getInt("chromosome");
+				DasType type=new DasType(typeId, "", "SO:0000694", "");
+				DasMethod method = null;
+				try {
+					method = new DasMethod("23AndMe", "microarray", "");
+				} catch (DataSourceException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				DasFeature feature = null;
+				try {
+					feature = new DasFeature(id,id,type, method,position, position,new Double(1),DasFeatureOrientation.ORIENTATION_NOT_APPLICABLE, DasPhase.PHASE_NOT_APPLICABLE,null, null, null, null, null  );
+				} catch (DataSourceException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				features.add(feature);
+			}
+			rs.close();
+			s.close();
+		} catch (SQLException e) {
+			try {
+				throw new DataSourceException("Problems executing the sql query", e);
+			} catch (DataSourceException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+		return features;
+	}
+
+	public ArrayList<DasType> getTypes() {
+		System.out.println("Getting types from genotype manager");
+		for (DasType type : types) {
+			System.out.println(type.getCvId());
+		}
+		return types;
+	}
+
+	public Integer getTotalCountForType(String typeId)
+			throws DataSourceException {
+
+		String sql = "SELECT count(bases) as num  from genotype where bases='"
+				+ typeId + "'";
+		int count = 0;
+		try {
+			Statement s = connection.createStatement();
+			s.executeQuery(sql);
+			ResultSet rs = s.getResultSet();
+			if (rs.next()) {
+				count = rs.getInt("num");
+			}
+			rs.close();
+			s.close();
+		} catch (SQLException e) {
+			throw new DataSourceException("Problems executing the sql query", e);
+		}
+
+		return count;
+	}
+
+
+	public String getDatabase() {
+		return database;
+	}
+
+
+}
