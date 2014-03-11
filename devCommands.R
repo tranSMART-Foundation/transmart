34976d3c46c4657174951e52e0926d6ae93000b2
Tim Dorscheidt <tim@thehyve.nl>
2014-03-11 15:44:19 +0100

    Removed use of development environment in devCommand.R
    Included manual package re-load command instead, as the previous method
    was not as reliable as I had hoped. Also included some other small
    changes.

--- devCommands.R
+++ devCommands.R
@@ -1,23 +1,27 @@
-# This file is added only as a development sandbox. It uses R's development
-# environment to load the package in isolation and contains some example calls.
+# This file contains some potentially useful development commands for 
+# re-installing the package, running some demo calls, and R-document creation
 
-# set dev environment and load package
-require(devtools)
-dev_mode(F)
-dev_mode(T)
-setwd("~/Projects/transmart-rclient")
-load_all("transmartRClient")
+# Unload+uninstall previous version, and install+load transmartRClient package from source
+detach("package:transmartRClient")
+remove.packages("transmartRClient")
+# Despite unloading and uninstalling the R client, you should restart your R session
+# to ensure removal of the individually loaded functions at this point
+install.packages("transmartRClient", repos = NULL, type = "source")
+require("transmartRClient")
 
 # Example steps to authenticate with, connect to, and retrieve data from tranSMART
-AuthenticateWithTransmart("test-build.thehyve.net")
-ConnectToTransmart("test-build.thehyve.net")
+AuthenticateWithTransmart("test-api.thehyve.net")
+ConnectToTransmart("test-api.thehyve.net")
 studies <- getStudies(as.data.frame = T)
 subjects <- getSubjects(studies$name[1], as.data.frame = T)
 observations <- getObservations(studies$name[1], subjects$id[1], as.data.frame = T)
-observations <- getObservations(studies$name[1], as.data.frame = T)
+observations <- getObservations(studies$name[2], as.data.frame = T)
 
 # create skeleton package to generate the Rd files per function
 setwd("~/Projects/transmart-rclient/transmartRClient/R/")
 sapply(list.files(), source)
-functionsToDocument <- c("AuthenticateWithTransmart", "ConnectToTransmart", "getStudies", "getSubjects", "getObservations")
-package.skeleton(list = functionsToDocument, name = "transmartRClient", path = "~/Projects/transmart-rclient/skeleton/")
+functionsToDocument <- c("AuthenticateWithTransmart", "ConnectToTransmart",
+                         "getStudies", "getSubjects", "getObservations")
+package.skeleton(list = functionsToDocument, name = "transmartRClient", 
+                 path = "~/Projects/transmart-rclient/skeleton/")
+
