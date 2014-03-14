5a4f7e03e6292f96f5571346b12e14866459bb32
Tim Dorscheidt <tim@thehyve.nl>
2014-03-14 13:06:06 +0100

    Cleaned up and clarified devCommands statements

--- devCommands.R
+++ devCommands.R
@@ -6,21 +6,22 @@ detach("package:transmartRClient")
 remove.packages("transmartRClient")
 # Despite unloading and uninstalling the R client, you should restart your R session
 # to ensure removal of the individually loaded functions at this point
-install.packages("transmartRClient", repos = NULL, type = "source")
+pathOfPackageSource <- "~/Projects/transmart-rclient/transmartRClient"
+install.packages(pathOfPackageSource, repos = NULL, type = "source")
 require("transmartRClient")
 
 # Example steps to authenticate with, connect to, and retrieve data from tranSMART
 AuthenticateWithTransmart("test-api.thehyve.net")
 ConnectToTransmart("test-api.thehyve.net")
-studies <- getStudies(as.data.frame = T)
-subjects <- getSubjects(studies$name[1], as.data.frame = T)
-observations <- getObservations(studies$name[1], subjects$id[1], as.data.frame = T)
+studies <- getStudies()
+subjects <- getSubjects(studies$name[2])
+observations <- getObservations(studies$name[2], subjects$id[1], as.data.frame = T)
 observations <- getObservations(studies$name[2], as.data.frame = T)
 
 # create skeleton package to generate the Rd files per function
 setwd("~/Projects/transmart-rclient/transmartRClient/R/")
 sapply(list.files(), source)
-functionsToDocument <- c("AuthenticateWithTransmart", "ConnectToTransmart",
+functionsToInclude <- c("AuthenticateWithTransmart", "ConnectToTransmart",
                          "getStudies", "getSubjects", "getObservations")
 package.skeleton(list = functionsToDocument, name = "transmartRClient", 
                  path = "~/Projects/transmart-rclient/skeleton/")
