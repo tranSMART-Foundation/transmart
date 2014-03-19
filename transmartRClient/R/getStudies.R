fe8bea3a133f768f1f6425da856ae1e4ea44edf8
Tim Dorscheidt <tim@thehyve.nl>
2014-03-19 11:33:00 +0100

    Added column culling to getStudies

--- transmartRClient/R/getStudies.R
+++ transmartRClient/R/getStudies.R
@@ -1,7 +1,7 @@
 # Copyright (c) 2014 The Hyve B.V.
 # This code is licensed under the GNU General Public License, version 3
 
-getStudies <- function(nameMatch = "", as.data.frame = TRUE) {
+getStudies <- function(name.match = "", as.data.frame = TRUE, cull.columns = TRUE) {
     .checkTransmartConnection()
 
     serverResult <- .transmartServerGetRequest("/studies", use.HAL = TRUE)
@@ -9,8 +9,16 @@ getStudies <- function(nameMatch = "", as.data.frame = TRUE) {
     
     studyNames <- sapply(listOfStudies, FUN = function(x) { x[["name"]] })
     names(listOfStudies) <- studyNames
-    listOfStudies <- listOfStudies[ grep(nameMatch, studyNames) ]
+    listOfStudies <- listOfStudies[ grep(name.match, studyNames) ]
 
-    if (as.data.frame) return(.listToDataFrame(listOfStudies))
+    if (as.data.frame) {
+        dataFrameStudies <- .listToDataFrame(listOfStudies)
+        if (cull.columns) {
+            columnsToKeep <- match(c("name", "api.link.self.href", "ontologyTerm.fullName"), names(dataFrameStudies))
+            return(dataFrameStudies[ , columnsToKeep])
+        }
+        return(dataFrameStudies)
+    }
+    
     listOfStudies
 }
