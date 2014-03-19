5afea0fe1e4411219b79c38b2a27f9f7040978db
Tim Dorscheidt <tim@thehyve.nl>
2014-03-19 11:32:42 +0100

    Added getConcepts function

---
+++ transmartRClient/R/getConcepts.R
@@ -0,0 +1,20 @@
+# Copyright (c) 2014 The Hyve B.V.
+# This code is licensed under the GNU General Public License, version 3
+
+getConcepts <- function(study.name, as.data.frame = TRUE, cull.columns = TRUE) {
+    .checkTransmartConnection()
+
+    serverResult <- .transmartServerGetRequest( paste("/studies/", study.name, "/concepts", sep=""), use.HAL = TRUE)
+    listOfConcepts <- serverResult$ontology_terms
+
+    if (as.data.frame) {
+      dataFrameConcepts <- .listToDataFrame(listOfConcepts)
+      if (cull.columns) {
+        columnsToKeep <- match(c("name", "fullName", "api.link.self.href"), names(dataFrameConcepts))
+        return(dataFrameConcepts[ , columnsToKeep])
+      }
+      return(dataFrameConcepts)
+    }
+
+    listOfConcepts
+}
