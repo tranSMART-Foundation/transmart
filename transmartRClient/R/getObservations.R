02615938a4cdbaebad6aa9978050f7294fe34d07
Tim Dorscheidt <tim@thehyve.nl>
2014-03-19 11:34:27 +0100

    getObservations filter changes and column culling added
    Removed filter by subjects, but added filter by concepts.
    Filter by subjects will be up to end-user for now, but is planned to be
    included in the near future.

--- transmartRClient/R/getObservations.R
+++ transmartRClient/R/getObservations.R
@@ -1,20 +1,38 @@
 # Copyright (c) 2014 The Hyve B.V.
 # This code is licensed under the GNU General Public License, version 3
 
-getObservations <- function(studyName, subjectID = NULL, as.data.frame = FALSE) {
+getObservations <- function(study.name, concept.match = NULL, concept.links = NULL, as.data.frame = TRUE) {
     .checkTransmartConnection()
 
-    if (is.null(subjectID)) {
-        serverResult <- .transmartServerGetRequest(
-            paste("/studies/", studyName, "/observations", sep=""),
-            use.HAL = TRUE)
+    if (!is.null(concept.match) && is.null(concept.links)) {
+        concept.links <- c()
+        studyConcepts <- getConcepts(study.name)
+        for (toMatch in concept.match) {
+            conceptMatch <- grep(toMatch, studyConcepts$name)[1] # TODO: this might result in the same concept being included multiple times
+            if (is.na(conceptMatch)) {
+                warning(paste("No match found for:", toMatch))
+            } else {
+                concept.links <- c(concept.links, studyConcepts$api.link.self.href[conceptMatch])
+            }
+        }
     } else {
-        serverResult <- .transmartServerGetRequest(
-            paste("/studies/", studyName, "/subjects/", subjectID, "/observations", sep=""),
-            use.HAL = TRUE) 
+        concept.links <- paste("/studies/", study.name, sep = "")
     }
-    listOfObservations <- serverResult$observations
-    
+
+    if (length(concept.links) == 0L) {
+        warning("No concepts selected or found to match your arguments.")
+        return(NULL)
+    }
+
+    listOfObservations <- list()
+
+    for (oneLink in concept.links) {
+        serverResult <- .transmartServerGetRequest(
+                paste(oneLink, "/observations", sep=""),
+                use.HAL = TRUE) 
+        listOfObservations <- c(listOfObservations, serverResult$observations)
+    }
+
     if (as.data.frame) return(.listToDataFrame(listOfObservations))
     listOfObservations
 }
