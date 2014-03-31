93e25df644664fdb895f72bcb18de9a6911ee0a0
Jan Kanis <jan.code@jankanis.nl>
2014-03-31 23:51:54 +0200

    Basic working parser
    The basic parser is now working and converts a protobuffer stream to
    a data frame.

--- readProtoBufData.R
+++ readProtoBufData.R
@@ -10,76 +10,81 @@ options("RProtoBuf.int64AsString" = TRUE)
 
 readProtoFiles('highdim.proto')
 
-f <- file('sample.protobuf', open='r+b')
-
-ff <- ConnectionInputStream(f)
-
 reset <- function() {
-  f <<- file('sample.protobuf', open='r+b')
-  ff <<- ConnectionInputStream(f)
+	f <<- file('sample.protobuf', open='r+b')
+	ff <<- ConnectionInputStream(f)
 }
+
+reset()
 
 run <- function() {
-  reset()
-  parseProtoBuf(ff)
+	reset()
+	parseProtoBuf(ff)
 }
 
-parseProtoBuf <- function(conn){
-	hdr <- readHeader(conn)
+# Convert a protobuf stream to a data frame
+parseProtoBuf <- function(conn) {
+	data <- readHeader(conn)
 	capacity <- 100
-	count <- 0
-  rows <- vector('list', capacity)
-  while(! is.null(hdrow <- readRow(conn))) {
-	  # geometric doubling
-	  if (count == capacity) {
-	    capacity <- capacity * 2
-			newrows <- vector('list', capacity)
-			newrows[1:count] <- rows
-			rows <- newrows
+	count <- length(data)
+	data <- c(data, vector('list', capacity - count))
+	while(! is.null(hdrow <- readRow(conn))) {
+		# geometric doubling
+		if (count == capacity) {
+			print(paste('doubling capacity from', capacity))
+			data <- c(data, vector('list', capacity))
+			capacity <- capacity * 2
 		}
 		count <- count + 1
-		rows[count] <- hdrow
-  }
-  messages <- list(header=hdr, rows=rows)
-	#toDataFrame(messages)
-  messages
+		data[count] <- hdrow
+		names(data)[count] <- names(hdrow)
+	}
+	data <- data[1:count]
+	do.call(data.frame, c(data, list(row.names='assayId')))
 }
 
 readHeader <- function(connection) {
-  size <- ReadVarint32(connection)
-  print(paste('size:', size))
-  msg <- ReadRaw(connection, size=size)
-  hdr <- read(highdim.HighDimHeader, msg)
-  typeName <- name(highdim.HighDimHeader$RowType$value(hdr$rowsType))
-  if(typeName != "DOUBLE") {
-    stop("HighDim data not of type DOUBLE")
-  }
-  hdr
+	size <- ReadVarint32(connection)
+	print(paste('header size:', size))
+	msg <- ReadRaw(connection, size=size)
+	hdr <- read(highdim.HighDimHeader, msg)
+	typeName <- name(highdim.HighDimHeader$RowType$value(hdr$rowsType))
+	if(typeName != "DOUBLE") {
+		stop("HighDim data not of type DOUBLE")
+	}
+
+	assays <- hdr$assay
+
+	fieldnames = c(
+		'assayId', 'patientId', 'sampleTypeName', 'timepointName',
+		'tissueTypeName', 'sampleCode')
+
+	assaydata <- lapply(fieldnames, function(f) vapply(assays, function(a) a[[f]], '') )
+	names(assaydata) <- fieldnames
+	return(assaydata)
 }
 
 readRow <- function(connection) {
-  size <- tryCatch(ReadVarint32(connection),
-                   error= function(e) NULL)
-  if (is.null(size)) {
-    return(NULL)
-  }
-  print(paste('size:', size))
-  msg <- ReadRaw(connection, size=size)
-  row <- read(highdim.Row, msg)
-  row
+	size <- tryCatch(ReadVarint32(connection),
+			 error= function(e) NULL)
+	if (is.null(size)) {
+		return(NULL)
+	}
+	print(paste('row size:', size))
+	msg <- ReadRaw(connection, size=size)
+	row <- read(highdim.Row, msg)
+	rowdata <- list(row$doubleValue)
+	names(rowdata) <- row$label
+	return(rowdata)
 }
 
 toDataFrame <- function(messages) {
-  header <- messages$header
+	header <- messages$header
 	assays <- messages$assay
-  rows <- messages$rows
-
-	fieldnames = c(
-	  'assayId', 'patientId', 'sampleTypeName', 'timepointName',
-		'tissueTypeName', 'sampleCode')
+	rows <- messages$rows
 
 	fields <- c(
-	    lapply(fieldnames, function(f) vapply(assay, function(a) a[[f]], '') )
+			lapply(fieldnames, function(f) vapply(assay, function(a) a[[f]], '') ),
 			lapply(rows, function(r) r$doubleValue)
 		)
 	names(fields) <- c(fieldnames, vapply(rows, function(r) { r$label }, '') )
