3a017d34e84739faf39baf6ef00293b4495e560c
Rianne Jansen <rianne@thehyve.nl>
2014-04-04 18:24:33 +0200

    *work-around to fix int64 variable issue (is only done if int64 variables cannot be read) *start with adjustments to be able to read highdim data of type GENERAL

--- readProtoBufData.R
+++ readProtoBufData.R
@@ -9,26 +9,32 @@ library(RProtoBuf)
 options("RProtoBuf.int64AsString" = TRUE)
 
 readProtoFiles('highdim.proto')
+#protoBufFile<-"Protobuf file samples//sample.protobuf" ##contains data of type DOUBLE
+protoBufFile<-"Protobuf file samples//mrna-all_data.protobuf" ##contains data of type GENERAL
 
-reset <- function() {
-	f <<- file('sample.protobuf', open='r+b')
+
+
+reset <- function(fileName) {
+	f <<- file(fileName, open='r+b')
 	ff <<- ConnectionInputStream(f)
 }
 
-reset()
 
-run <- function() {
-	reset()
+run <- function(fileName) {
+	reset(fileName)
 	parseProtoBuf(ff)
 }
 
 # Convert a protobuf stream to a data frame
 parseProtoBuf <- function(conn) {
 	data <- readHeader(conn)
-	capacity <- 100
+  dataType<-data[["dataType"]]
+  data<-data[["assaydata"]]
+ 
+  capacity <- 100
 	count <- length(data)
 	data <- c(data, vector('list', capacity - count))
-	while(! is.null(hdrow <- readRow(conn))) {
+	while(! is.null(hdrow <- readRow(conn, dataType))) {
 		# geometric doubling
 		if (count == capacity) {
 			print(paste('doubling capacity from', capacity))
@@ -40,8 +46,9 @@ parseProtoBuf <- function(conn) {
 		names(data)[count] <- names(hdrow)
 	}
 	data <- data[1:count]
-	do.call(data.frame, c(data, list(row.names='assayId')))
+	#do.call(data.frame, c(data, list(row.names='assayId')))
 }
+
 
 readHeader <- function(connection) {
 	size <- ReadVarint32(connection)
@@ -49,9 +56,9 @@ readHeader <- function(connection) {
 	msg <- ReadRaw(connection, size=size)
 	hdr <- read(highdim.HighDimHeader, msg)
 	typeName <- name(highdim.HighDimHeader$RowType$value(hdr$rowsType))
-	if(typeName != "DOUBLE") {
-		stop("HighDim data not of type DOUBLE")
-	}
+	#if(typeName != "DOUBLE") {
+	#	stop("HighDim data not of type DOUBLE")
+	#}
 
 	assays <- hdr$assay
 
@@ -59,12 +66,35 @@ readHeader <- function(connection) {
 		'assayId', 'patientId', 'sampleTypeName', 'timepointName',
 		'tissueTypeName', 'sampleCode')
 
-	assaydata <- lapply(fieldnames, function(f) vapply(assays, function(a) a[[f]], '') )
+	assaydata<-tryCatch(lapply(fieldnames, function(f) vapply(assays, function(a) a[[f]], '') ), 
+           error = function(e) getAssayDataAvoidingInt64Issue(assays, fieldnames))
+  
 	names(assaydata) <- fieldnames
-	return(assaydata)
+	#return(lassaydata)	
+	return(list(assaydata=assaydata, dataType=typeName))
 }
 
-readRow <- function(connection) {
+
+#workaround to catch problems with Int64 values. In some cases int64 types are unsupported and an int64 type value cannot be directly accessed. 
+getAssayDataAvoidingInt64Issue <- function(assays, fieldnames)
+{
+  lapply(fieldnames, 
+         function(f) vapply(assays, FUN=getField, FUN.VALUE="c", fieldname=f) )
+  
+  
+}
+
+getField<-function(assayDescr,fieldname )
+{
+  assayAsStrings<-strsplit(as.character(assayDescr),split="\n")[[1]]
+  desiredFieldString<-grep(fieldname, assayAsStrings, value=T)
+  separatedString<-scan(text=desiredFieldString, what="c", quiet=T)
+  if(length(separatedString)!=2){stop(paste("error: could not retrieve value of field", fieldname))}
+  desiredField<-separatedString[2]
+  return(desiredField)
+}
+
+readRow <- function(connection, dataType) {
 	size <- tryCatch(ReadVarint32(connection),
 			 error= function(e) NULL)
 	if (is.null(size)) {
@@ -73,9 +103,35 @@ readRow <- function(connection) {
 	print(paste('row size:', size))
 	msg <- ReadRaw(connection, size=size)
 	row <- read(highdim.Row, msg)
-	rowdata <- list(row$doubleValue)
-	names(rowdata) <- row$label
-	return(rowdata)
+  
+  if(dataType=="DOUBLE")
+  {
+    rowdata <- list(row$doubleValue) #for double values
+    names(rowdata) <- row$label
+    #return(rowdata) HERE???
+  }
+  
+  if(dataType=="GENERAL")
+  {
+    #print(c("msg:",msg))
+    print(c("row:",row))
+    write(c("rowTostring:", toString(row)),"") ##for now we don't do anything with the biomarker info. Include?
+    rowMsg<-row$mapValue #for general values
+    #print(c("rowMsg",rowMsg))
+    mapValues<-list()
+    
+    for(i in 1:length(rowMsg))
+    {
+      mapValues<-c(mapValues,rowMsg[[i]][["value"]])
+    }
+    
+   # GIVE LABEL TO THE MAPVALUES (as done for the data with DOUBLE values; names(rowdata)<-row$label )
+  #  DEFINE ROWDATA so it can be returned
+    
+  }  
+
+  return(rowdata) #MAKE SURE RECEIVING FUNCTION (where it returns the value TO) CAN DO SOMETHING WITH THE rowoutput for "GENERAL" data. 
+                  #ADAPT THE RECEIVING FUNCTION SO IT CAN MAKE A DATAFRAME OUT OF THE DATA (split the 4 elements (values) per map so they make up 4 rows/columns?)
 }
 
 toDataFrame <- function(messages) {
