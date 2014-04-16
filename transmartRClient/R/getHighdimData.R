2f08add679be3143e4c43c291ad90dabef5f8ec8
Tim Dorscheidt <tim@thehyve.nl>
2014-04-16 16:43:39 +0200

    :duck:
    Whitespace cleaned up. And small print statement changes.

--- transmartRClient/R/getHighdimData.R
+++ transmartRClient/R/getHighdimData.R
@@ -3,9 +3,9 @@
 # version 3, or (at your option) any later version.
 
 getHighdimData <- function(study.name, concept.match = NULL, concept.link = NULL, projection = NULL) {
-    require("RProtoBuf")    
+    require("RProtoBuf")
     .checkTransmartConnection()
-    
+
     if (is.null(concept.link) && !is.null(concept.match)) {
         studyConcepts <- getConcepts(study.name)
         conceptFound <- grep(concept.match, studyConcepts$name)[1]
@@ -13,31 +13,29 @@ getHighdimData <- function(study.name, concept.match = NULL, concept.link = NULL
             warning(paste("No match found for:", concept.match))
         } else { concept.link <- studyConcepts$api.link.self.href[conceptFound] }
     }
-    
+
     if (length(concept.link) == 0L) {
         warning("No concepts selected or found to match your arguments.")
         return(NULL)
     }
-    
-    serverResult <- .transmartServerGetRequest(
-        paste(concept.link, "/highdim", sep=""),
-        accept.type = "hal")
+
+    serverResult <- .transmartServerGetRequest(paste(concept.link, "/highdim", sep=""), accept.type = "hal")
     if (length(serverResult$dataTypes) == 0) {
         warning("This high dimensional concept contains no data.")
         return(NULL)
     }
     listOfHighdimDataTypes <- serverResult$dataTypes[[1]]
-    
+
     if (!is.null(projection)) {
         matchingProjectionIndex <- which(names(listOfHighdimDataTypes$api.link) == projection)
         if (length(matchingProjectionIndex) > 0) {
             projectionLink <- listOfHighdimDataTypes$api.link[[matchingProjectionIndex]]
         } else { projection <- NULL }
     } else { projection <- NULL }
-    
+
     if (is.null(projection)) {
         cat("No valid projection selected.\nSet the projection argument to one of the following options:\n ")
-        cat(paste(listOfHighdimDataTypes$supportedProjections,"\n"))
+        cat(paste(listOfHighdimDataTypes$supportedProjections, "\n"))
         return(listOfHighdimDataTypes$supportedProjections)
     }
     cat("Retrieving data from server.", as.character(Sys.time()), "\n")
@@ -52,36 +50,35 @@ getHighdimData <- function(study.name, concept.match = NULL, concept.link = NULL
 
 .parseHighdimData <- function(rawVector) {
     require('hash')
-    
+
     protoFileLocation <- system.file("extdata", "highdim.proto", package="transmartRClient")
     readProtoFiles(protoFileLocation)
-    
+
     dataChopper <- .messageChopper(rawVector)
-    
+
     message <- dataChopper$getNextMessage()
     header <- read(highdim.HighDimHeader, message)
     columnSpec <- header$columnSpec
     assays <- header$assay
     DOUBLE <- highdim.ColumnSpec$ColumnType$DOUBLE
     STRING <- highdim.ColumnSpec$ColumnType$STRING
-    
+
     columns <- .expandingList(1000)
-    
+
     noAssays <- length(assays)
-    
+
     assayLabels <- .DollarNames(assays[[1]])[1:length(assays[[1]])]
-    
+
     for (label in assayLabels) {
         if (label == "assayId") {
             columns$add(label, sapply(assays,
-                                      function(a) sub("assayId: ", "", grep(label, strsplit(as.character(a), split = "\n")[[1]], value = TRUE))))
+                    function(a) sub("assayId: ", "", grep(label, strsplit(as.character(a), split = "\n")[[1]], value = TRUE))))
         } else {
             columns$add(label, sapply(assays, function(a) a[[label]]))
         }
     }
     
-    cat(paste("Received data for",noAssays,"assays. Unpacking data and converting to data.frame.",as.character(Sys.time()),"\n"))
-    
+    cat(paste("Received data for", noAssays, "assays. Unpacking data.", as.character(Sys.time()), "\n"))
 
     pb <- txtProgressBar(min = 0, max = length(rawVector), style = 3)
     labelToBioMarker <- hash() #biomarker info is optional, but should not be omitted, as it is also part of the data
@@ -100,7 +97,7 @@ getHighdimData <- function(study.name, concept.match = NULL, concept.link = NULL
             columns$add(rowlabel, rowValues[[1]]$doubleValue)
             next
         }
-        
+
         for(i in 1:length(rowValues)) {
             entryName <- paste(rowlabel, columnSpec[[i]]$name, sep=".")
             if(columnSpec[[i]]$type == STRING) {
@@ -112,19 +109,17 @@ getHighdimData <- function(study.name, concept.match = NULL, concept.link = NULL
         
     }
     close(pb) 
-    
+
+    cat("Data unpacked. Converting to data.frame.", as.character(Sys.time()),"\n")
     data <- as.data.frame(columns$as.list(), stringsAsFactors=FALSE)
-    
-    cat("Data unpacked.", as.character(Sys.time()),"\n")
-    
-    
+
     if(all(is.na(values(labelToBioMarker)))) {
         cat("No biomarker information available.")
-        labelToBioMarker<-"No biomarker information is available for this dataset"
-        return(list(data=data))
+        labelToBioMarker <- "No biomarker information is available for this dataset"
+        return(list(data = data))
     } else {
-        cat("Additional biomarker information is available. Returning a list containing the high dimensional data and a hash describing which (column) labels refer to which bioMarker\n")
-        return(list(data=data, labelToBioMarkerMap = labelToBioMarker))
+        cat("Additional biomarker information is available.\nThe result will be a list containing the high dimensional data and a hash describing which (column) labels refer to which bioMarker\n")
+        return(list(data = data, labelToBioMarkerMap = labelToBioMarker))
     }
 }
 
@@ -132,9 +127,9 @@ getHighdimData <- function(study.name, concept.match = NULL, concept.link = NULL
     msbSetToOne <- as.raw(128)
     progressTotal <- length(rawVector)
     pb <- c()
-    
+
     getNextMessage <- function() {
-        if (endOfLastMessage >= length(rawVector)) {return(NULL)}
+        if (endOfLastMessage >= length(rawVector)) { return(NULL) }
         varint32Size <- max(which((msbSetToOne & rawVector[(endOfLastMessage+1):(endOfLastMessage+5)]) > 0)) + 1
         varint32Connection <- rawConnection(rawVector[(endOfLastMessage+1):(endOfLastMessage+varint32Size)], open = "r+b")
         class(varint32Connection) <- "connection"
@@ -147,9 +142,9 @@ getHighdimData <- function(study.name, concept.match = NULL, concept.link = NULL
         endOfLastMessage <<- endOfThisMessage
         return(message)
     }
-    
+
     getRawVectorIndex <- function() { return(endOfLastMessage) }
-    
+
     return(list(getNextMessage = getNextMessage, getRawVectorIndex = getRawVectorIndex))
 }
 
@@ -158,35 +153,35 @@ getHighdimData <- function(study.name, concept.match = NULL, concept.link = NULL
 # or element setting this would lead to a quadratic number of memory copies and 
 # thus a quadratic runtime. To prevent that, this function implements a bare
 # bones expanding array, in which list additions are (amortized) constant time.
-.expandingList <- function(capacity=10) {
+.expandingList <- function(capacity = 10) {
     buffer <- vector('list', capacity)
     names <- character(capacity)
     count <- 0
-    
+
     methods <- list()
-    
+
     methods$double.size <- function() {
         buffer <<- c(buffer, vector('list', capacity))
         names <<- c(names, character(capacity))
         capacity <<- capacity * 2
     }
-    
+
     methods$add <- function(name, val) {
         if(count == capacity) {
             methods$double.size()
         }
-        
+
         count <<- count + 1
         buffer[[count]] <<- val
         names[count] <<- name
     }
-    
+
     methods$as.list <- function() {
         b <- buffer[0:count]
         names(b) <- names[0:count]
         return(b)
     }
-    
+
     methods
 }
 
