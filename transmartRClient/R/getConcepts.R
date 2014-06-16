ad272a29f3f0166dc31ab64f37f34fda13d9cfbf
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-06-16 16:39:31 +0200

    New copyright headers

--- transmartRClient/R/getConcepts.R
+++ transmartRClient/R/getConcepts.R
@@ -1,6 +1,25 @@
-# Copyright (c) 2014 The Hyve B.V.
-# This code is licensed under the GNU General Public License,
-# version 3, or (at your option) any later version.
+# Copyright 2014 Janssen Research & Development, LLC.
+#
+# This file is part of tranSMART R Client: R package allowing access to
+# tranSMART's data via its RESTful API.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version, along with the following terms:
+#
+#   1. You may convey a work based on this program in accordance with
+#      section 5, provided that you retain the above notices.
+#   2. You may convey verbatim copies of this program code as you receive
+#      it, in any medium, provided that you retain the above notices.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
 
 getConcepts <- function(study.name, as.data.frame = TRUE, cull.columns = TRUE) {
     .checkTransmartConnection()
