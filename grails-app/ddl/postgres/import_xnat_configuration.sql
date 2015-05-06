bfd080ef7bdbce1e3a4cb0f06f540dd1bfe8f4c4
Erwin Vast <e.vast@erasmusmc.nl>
2015-05-06 10:29:33 +0200

    append database name to queries

--- grails-app/ddl/postgres/import_xnat_configuration.sql
+++ grails-app/ddl/postgres/import_xnat_configuration.sql
@@ -1,7 +1,7 @@
 --
 -- Name: import_xnat_configuration; Type: TABLE; Schema: searchapp; Owner: -
 --
-CREATE TABLE import_xnat_configuration (
+CREATE TABLE searchapp.import_xnat_configuration (
     id bigint NOT NULL,
     version bigint NOT NULL,
     name character varying(255) NOT NULL,
@@ -11,9 +11,11 @@ CREATE TABLE import_xnat_configuration (
     project character varying(255) NOT NULL,
     node character varying(255) NOT NULL
 );
+ALTER TABLE searchapp.import_xnat_configuration OWNER TO searchapp;
+GRANT ALL ON TABLE searchapp.import_xnat_configuration TO searchapp;
 
 --
 -- Name: pk_import_xnat_configuration; Type: CONSTRAINT; Schema: searchapp; Owner: -
 --
-ALTER TABLE ONLY import_xnat_configuration
+ALTER TABLE ONLY searchapp.import_xnat_configuration
     ADD CONSTRAINT pk_import_xnat_configuration PRIMARY KEY (id)
--- grails-app/ddl/postgres/import_xnat_variable.sql
+++ grails-app/ddl/postgres/import_xnat_variable.sql
@@ -1,24 +1,26 @@
 --
 -- Name: import_xnat_variable; Type: TABLE; Schema: searchapp; Owner: -
 --
-CREATE TABLE import_xnat_variable (
+CREATE TABLE searchapp.import_xnat_variable (
     id bigint NOT NULL,
     configuration_id bigint NOT NULL,
     name character varying(255) NOT NULL,
     datatype character varying(255) NOT NULL,
     url character varying(255) NOT NULL
 );
+ALTER TABLE searchapp.import_xnat_variable OWNER TO searchapp;
+GRANT ALL ON TABLE searchapp.import_xnat_variable TO searchapp;
 
 --
 -- Name: pk_import_xnat_var; Type: CONSTRAINT; Schema: searchapp; Owner: -
 --
-ALTER TABLE ONLY import_xnat_variable
+ALTER TABLE ONLY searchapp.import_xnat_variable
     ADD CONSTRAINT pk_import_xnat_variable PRIMARY KEY (id);
 
 --
 -- Name: fk_import_xnat_var_to_config; Type: FK CONSTRAINT; Schema: searchapp; Owner: -
 --
-ALTER TABLE ONLY import_xnat_variable
-    ADD CONSTRAINT fk_import_xnat_var_to_config FOREIGN KEY (configuration_id) REFERENCES import_xnat_configuration(id);
+ALTER TABLE ONLY searchapp.import_xnat_variable
+    ADD CONSTRAINT fk_import_xnat_var_to_config FOREIGN KEY (configuration_id) REFERENCES searchapp.import_xnat_configuration(id);
 
 ;
