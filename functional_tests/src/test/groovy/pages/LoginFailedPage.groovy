f860fa6df0ba94c1f47cf9f54ba8e6ccef80292d
Terry Weymouth <weymouth@umich.edu>
2015-05-07 12:33:24 -0400

    Fixed minor bog in LoginTests: is test is repeated withing 10 min, account is locked, and another error message is displayed

--- functional_tests/src/test/groovy/pages/LoginFailedPage.groovy
+++ functional_tests/src/test/groovy/pages/LoginFailedPage.groovy
@@ -4,7 +4,9 @@ import pages.LoginPage
 
 class LoginFailedPage extends LoginPage {
     static at = {
-        topMessage == 'Please login...' &&
+        topMessage == 'Please login...' && (
                 errorMessage.contains('Login has failed')
+                || errorMessage.contains('Your account has been locked')
+        )
     }
 }
