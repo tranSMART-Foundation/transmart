57ffac191f2e81152e1c82e0590bd2583d610b8f
Peter Rice <p.rice@imperial.ac.uk>
2015-05-07 14:10:18 +0100

    utility functions

---
+++ functional_tests/src/test/groovy/functions/Utilities.groovy
@@ -0,0 +1,72 @@
+package functions
+
+import geb.junit4.GebReportingTest
+import junit.framework.AssertionFailedError
+
+import geb.Module
+import geb.Page
+
+
+import pages.Constants
+import pages.LoginPage
+
+import pages.modules.CommonHeaderModule
+
+import pages.LoginPage
+
+/**
+ * Created by price on 19-12-2014.
+ */
+
+class Utilities extends GebReportingTest {
+    
+    void toLandingPage() {
+
+        via Constants.LANDING_PAGE.class
+
+        if(at(LoginPage)) {
+
+            usernameField.value Constants.GOOD_USERNAME
+            passwordField.value Constants.GOOD_PASSWORD
+            loginButton.click()
+
+            assert at(Constants.LANDING_PAGE.class)
+        }
+
+    }
+
+    void login(Class<? extends Page> redirectionPage) {
+
+        usernameField.value Constants.GOOD_USERNAME
+        passwordField.value Constants.GOOD_PASSWORD
+
+        loginButtonNoTo.click()
+
+        at(redirectionPage)
+    }
+
+    void goToPageMaybeLogin(Class<? extends Page> page, boolean firstCall = true) {
+        via page
+
+        if (isAt(page)) {
+            /* Already at required page */
+            return
+        } else if (isAt(LoginPage)) {
+            /* At login page - login with user/password */
+            login(page)
+        } else if (isAt(Constants.LANDING_PAGE.class)) {
+            /* Unexpectedly at landing page when not wanted */
+            if (!firstCall) {
+                throw new AssertionFailedError('Redirection loop')
+            }
+            /* if auto-login is on, we're unfortunately forwarded here */
+            goToPageMaybeLogin(page, false)
+        } else {
+            throw new AssertionFailedError(
+                    "Expected to be at either the LoginPage, $Constants.LANDING_PAGE or $page")
+        }
+    }
+
+}
+
+
