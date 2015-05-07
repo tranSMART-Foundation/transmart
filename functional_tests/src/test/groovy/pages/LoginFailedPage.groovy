7034864a0300fa297afce6150a1e7159c16c0356
Terry Weymouth <weymouth@umich.edu>
2015-05-07 12:40:44 -0400

    added comment to explain (mostly) redundent second error message test on LoginFailedPage

--- functional_tests/src/test/groovy/pages/LoginFailedPage.groovy
+++ functional_tests/src/test/groovy/pages/LoginFailedPage.groovy
@@ -3,6 +3,7 @@ package pages
 import pages.LoginPage
 
 class LoginFailedPage extends LoginPage {
+    // locked error message occures in rare case that bad login is repeated within 10 min
     static at = {
         topMessage == 'Please login...' && (
                 errorMessage.contains('Login has failed')
