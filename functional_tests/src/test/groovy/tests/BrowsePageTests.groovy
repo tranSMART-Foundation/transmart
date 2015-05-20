6c151d075d739d2005b1c0caeeefd6874d53c8f9
Peter Rice <p.rice@imperial.ac.uk>
2015-05-20 16:24:58 +0100

    Separate tests for Utilities - using Spock

--- functional_tests/src/test/groovy/tests/BrowsePageTests.groovy
+++ functional_tests/src/test/groovy/tests/BrowsePageTests.groovy
@@ -4,11 +4,8 @@ import static org.hamcrest.Matchers.*
 import geb.junit4.GebReportingTest
 
 import org.junit.Test
-import org.junit.Ignore
 
 import pages.modules.CommonHeaderModule
-
-import functions.Utilities
 
 import functions.Constants
 
@@ -18,9 +15,8 @@ import pages.BrowseResultsPage
 
 class BrowsePageTests extends GebReportingTest {
 
-    //TODO: tests with Ignore need to be fixed
-
-    def util = new Utilities()
+    // Assume a standard set of browse tab metadata has been loaded
+    // using transmart-data and supplied sql loading scripts
 
     @Test
     void BrowseTab() {
@@ -31,40 +27,5 @@ class BrowsePageTests extends GebReportingTest {
 
     }
 
-    // perhaps this test should be moved to a new Test file: UtilityTabTests ??
-    @Ignore
-    @Test
-    void BrowseTabUtilities() {
-
-        util.goToPageMaybeLogin(BrowsePage)
-
-        assert at(BrowsePage)
-
-        commonHeader { module CommonHeaderModule }
-
-        commonHeader.tableMenuUtilities.click()
-
-        // check the Utilities menu has the expected content
-        commonHeader.utilitiesList()
-
-        assert commonHeader.utilitiesHelp()
-        assert commonHeader.utilitiesContact()
-        assert commonHeader.utilitiesLogout()
-        assert commonHeader.utilitiesPassword()
-        assert commonHeader.utilitiesAbout()
-
-        commonHeader.utilitiesDoLogout()
-
-        assert at(LoginPage)
-
-        usernameField.value Constants.GOOD_USERNAME
-        passwordField.value Constants.GOOD_PASSWORD
-
-        //this tests for the landing page or a failure
-        loginButton.click()
-
-        //make sure it was a success!
-        assert at(Constants.LANDING_PAGE.class)
-    }
 
 }
---
+++ functional_tests/src/test/groovy/tests/UtilityTabTests.groovy
@@ -0,0 +1,72 @@
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.*
+
+import geb.junit4.GebReportingTest
+
+import org.junit.Test
+
+import pages.modules.CommonHeaderModule
+
+import functions.Utilities
+
+//import functions.Constants
+
+import pages.LoginPage
+import pages.BrowsePage
+import pages.BrowseResultsPage
+
+import geb.spock.GebSpec
+import geb.spock.GebReportingSpec
+import spock.lang.Stepwise
+
+@Stepwise
+class UtilityTabSpec extends GebReportingSpec {
+
+    def "start on the Browse tab"() {
+        def util = new Utilities();
+
+        when:
+        util.goToPageMaybeLogin(BrowsePage)
+
+
+        then:
+        assert at(BrowsePage)
+
+    }
+
+    def "click on the Utilities tab"() {
+        commonHeader { module CommonHeaderModule }
+
+        when:
+        commonHeader.tableMenuUtilities.click()
+
+        then:
+        commonHeader.utilitiesMenuSize() == 5
+        commonHeader.utilitiesHelp()
+        commonHeader.utilitiesContact()
+        commonHeader.utilitiesLogout()
+        commonHeader.utilitiesPassword()
+        commonHeader.utilitiesAbout()
+    }
+
+    def "logout gives login page"() {
+        when:
+        commonHeader.utilitiesDoLogout()
+
+        then:
+        assert at(LoginPage)
+    }
+    
+    def "login as admin"() {
+        when:
+        usernameField.value Constants.ADMIN_USERNAME
+        passwordField.value Constants.ADMIN_PASSWORD
+
+        loginButton.click()
+
+        then:
+        assert at(Constants.LANDING_PAGE.class)
+
+    }
+
+}
