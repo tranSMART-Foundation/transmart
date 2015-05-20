e6dc99440573404dcf129f2e9c042658f9d3d1ab
Peter Rice <p.rice@imperial.ac.uk>
2015-05-20 16:23:27 +0100

    explicit admin user/password

--- functional_tests/src/test/groovy/functions/Constants.groovy
+++ functional_tests/src/test/groovy/functions/Constants.groovy
@@ -9,6 +9,8 @@ class Constants {
     public static final String BAD_PASSWORD = 'bad password'
     public static final String GOOD_USERNAME = 'admin'
     public static final String GOOD_PASSWORD = 'admin'
+    public static final String ADMIN_USERNAME = 'admin'
+    public static final String ADMIN_PASSWORD = 'admin'
 
     public static final boolean AUTO_LOGIN_ENABLED = false
     public static final Page LANDING_PAGE = new BrowsePage()
