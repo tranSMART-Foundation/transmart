411d2342dee20d2678b96b1e0fcc90fdb2f221cd
Peter Rice <p.rice@imperial.ac.uk>
2015-05-22 10:10:04 +0100

    autologin false by default

--- functional_tests/src/test/groovy/functions/Constants.groovy
+++ functional_tests/src/test/groovy/functions/Constants.groovy
@@ -12,7 +12,7 @@ class Constants {
     public static final String ADMIN_USERNAME = 'admin'
     public static final String ADMIN_PASSWORD = 'admin'
 
-    public static final boolean AUTO_LOGIN_ENABLED = true // locally set to true
+    public static final boolean AUTO_LOGIN_ENABLED = false // locally set to true
     public static final Page LANDING_PAGE = new BrowsePage() // local configuration
 
     public static final String GSE8581_KEY = '\\\\Public Studies\\Public Studies\\GSE8581\\'
