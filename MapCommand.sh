ce4714dcba84e680ee6ca1db282768c53eff68ec
Terry Weymouth <weymouth@umich.edu>
2015-09-16 11:35:16 -0400

    Update MapCommand.sh

--- MapCommand.sh
+++ MapCommand.sh
@@ -26,24 +26,39 @@ transmart-test \
 echo "Mapping Command . . ." > $out
 
 foreach dir ($dirs)
-	pushd $dir >> $out
-		echo "" >> $out
+	pushd $dir
 		echo "" >> $out
 		echo "++ $dir ++" >> $out
 		echo "++ $dir ++"
-		git fetch transmart
+# -------------- actual commands
+		git checkout release-1.2.5
 		git diff --name-only transmart/master >> $out
+		git status >> $out
+# -------------- actual commands		
 		echo "-- $dir --" >> $out
-		echo "" >> $out
-	popd >> $out
+	popd
 end
 
 echo "DONE!"
 
+# various log options
 # git log --pretty=oneline v1.2.3...HEAD >> $out
 # git log --pretty=full v1.2.3...HEAD >> $out
 # git shortlog -s -n --all v1.2.3...HEAD >> $out
+
+# various checking commands
+# git status >> $out
+
+# initial clone
 # git clone git@github.com:tranSMART-Foundation/{$dir}.git >> $out
+
+# to set remote = transmart
 # git remote add transmart git@github.com:transmart/{$dir}.git >> $out
-# git fetch transmart
-# git diff --name-only transmart/master >> $out
+
+# to check diff with remote = transmart - already defined
+#		git fetch transmart
+#		git diff --name-only transmart/master >> $out
+
+# create release branch
+#		git checkout -b release-1.2.5
+#		git push -u origin release-1.2.5
