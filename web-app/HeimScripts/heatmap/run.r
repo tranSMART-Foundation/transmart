b1c6ffcdff975f141e15783e2bb4bf6f7ba7635d
Piotr Zakrzewski <piotr@thehyve.nl>
2015-10-14 19:21:28 +0200

    Fix dataframe retrieval from loaded_variables

--- web-app/HeimScripts/heatmap/run.r
+++ web-app/HeimScripts/heatmap/run.r
@@ -1,34 +1,18 @@
-#global_register <-list(rowNames = c("a","b"),
-#                       columnNames = c("1","2"),
-#                       origin = c(1,2),
-#                       data = matrix(c(35,700),nrow=2,ncol=2)
-#                       )
-
 library(gplots)
-# This script expects global_register to be populated before execution
-# see lines above for an example
-data.row.names <- global_register["rowNames"][[1]]
-data.col.names <- global_register["columnNames"][[1]]
-data.origin <- global_register["origin"][[1]]
-data.matrix <- global_register["data"][[1]]
 
-rownames(data.matrix) <- unlist(data.row.names)
-colnames(data.matrix) <- unlist(data.col.names)
+dataset <- loaded_variables[[1]] #dataframe with columns: Row.Label, Bio.marker, ASSAY_0001 ASSAY_0002 ...
 
-#For now we assume only 2 groups. 1 and 2
-color.groups <- function(x){
-  if(x == 1){
-    return("coral3") # R color
-  }else{
-    return("chartreuse3")
-  }
-}
+measurements  <- subset(dataset,select=-c(Row.Label,Bio.marker)) # this will select all columns other than Probe and Biomarker columns
+measurements  <- data.matrix(measurements)
 
-groups <- lapply(data.origin, color.groups)
-groups <- unlist(groups)
+measurements <- log(measurements,2)
+
+rownames(measurements) <- df$Row.Label
+
+
 
 png(filename="heatmap.png",width = 800,height=800)
-heatmap.2(data.matrix,
+heatmap.2(measurements,
           scale = "none",
           dendrogram = "none",
           Rowv = NA,
@@ -36,8 +20,8 @@ heatmap.2(data.matrix,
           density.info = "none", # histogram", # density.info=c("histogram","density","none")
           trace = "none",
           col=redgreen(75),
-          margins=c(12,12),
-          ColSideColors= as.character(groups)
+          margins=c(12,12)
+          #ColSideColors= as.character(groups)
           #adjCol=c("left","top")
 )
 dev.off()
