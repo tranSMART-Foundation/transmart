6742025c300035e45faf94a643f79979e7c674f8
Piotr Zakrzewski <piotr@thehyve.nl>
2015-10-15 11:46:49 +0200

    Indentation.

--- web-app/HeimScripts/heatmap/run.r
+++ web-app/HeimScripts/heatmap/run.r
@@ -2,17 +2,17 @@ library(gplots)
 
 
 main <- function(){
-dataset <- loaded_variables[[1]] #dataframe with columns: Row.Label, Bio.marker, ASSAY_0001 ASSAY_0002 ...
+    dataset <- loaded_variables[[1]] #dataframe with columns: Row.Label, Bio.marker, ASSAY_0001 ASSAY_0002 ...
 
-measurements  <- subset(dataset,select=-c(Row.Label,Bio.marker)) # this will select all columns other than Row.Label,Bio.marker columns
-measurements  <- data.matrix(measurements)
+    measurements  <- subset(dataset,select=-c(Row.Label,Bio.marker)) # this will select all columns other than Row.Label,Bio.marker columns
+    measurements  <- data.matrix(measurements)
 
-measurements <- log(measurements,2)
+    measurements <- log(measurements,2)
 
-rownames(measurements) <- df$Row.Label
+    rownames(measurements) <- df$Row.Label
 
-png(filename="heatmap.png",width = 800,height=800)
-heatmap.2(measurements,
+    png(filename="heatmap.png",width = 800,height=800)
+    heatmap.2(measurements,
           scale = "none",
           dendrogram = "none",
           Rowv = NA,
@@ -23,7 +23,7 @@ heatmap.2(measurements,
           margins=c(12,12)
           #ColSideColors= as.character(groups)
           #adjCol=c("left","top")
-)
-dev.off()
+    )
+    dev.off()
 }
 
