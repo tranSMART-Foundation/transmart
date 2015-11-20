56bf1dae80d803fb1efc313c60bcfa888445040c
Piotr Zakrzewski <piotr@thehyve.nl>
2015-11-20 14:12:37 +0100

    :hankey:

--- web-app/HeimScripts/heatmap/run.r
+++ web-app/HeimScripts/heatmap/run.r
@@ -2,18 +2,31 @@ library(jsonlite)
 library(reshape2)
 
 
-main <- function(max_rows=50){
-  df <- loaded_variables[[1]] # SmartR does not support multiple HDD nodes yet
+
+
+main <- function(max_rows=100){
+  df <- loaded_variables[[length(loaded_variables)]] # SmartR does not support multiple HDD nodes yet
+  # We take the last df to alleviate bug with new HDDs dropped being ignored - later on we will use label name explicitly.
   if(ncol(df) > 3){
-    variances <- apply(df[,3:ncol(df)],1,var, na.rm = T) # Calculating variance per probe
+  #this is the case for more than 1 sample
+    variances <- apply(df[,3:ncol(df)],1,var, na.rm = T) # Calculating variance per probe (per row)
+    means <- rowMeans(df[,3:ncol(df)], na.rm = T) # this is just an auxiliary column - it will not be used for JSON.
+    sdses <- apply(df[,3:ncol(df)],1,sd, na.rm = T) # this is just an auxiliary column - it will not be used for JSON.
+    df["MEAN"] <- means
+    df["SD"] <- sdses
     df["SIGNIFICANCE"] <- variances
-    df["MEAN"] <- rowMeans(df[,3:ncol(df)], na.rm = T)
-    df["SD"] <- apply(df[,3:ncol(df)],1,sd, na.rm = T)
     df <- df[with(df, order(-SIGNIFICANCE)), ]
   }
   else{
-    df["SIGNIFICANCE"] <- rep(1.0, nrow(df))
+  #one sample
+    variance <- 1.0 # We cannot get any significance measure for just one sample
+    mean <- mean(ex_df[,3], na.rm = T) #For just one sample we take mean over all genes (whole column), not per gene
+    sds <- sd(df[,3],na.rm = T) #For just one sample we take mean over all genes (whole column), not per gene
+    df["MEAN"] <- rep(mean,nrow(df))
+    df["SD"] <- rep(sds,nrow(df))
+    df["SIGNIFICANCE"] <- rep(variance, nrow(df))
   }
+
   df <- df[1:max_rows,]
   fields <- buildFields(df)
   geneSymbols <- unique(fields["GENESYMBOL"])[,1] #[,1] in order to get a vector, otherwise we get a dataframe
