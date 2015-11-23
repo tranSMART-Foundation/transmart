95bd7c703a7d500b78fee5dc4b368d9908ad9ea6
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2015-11-23 18:12:25 +0100

    Fix subsequent fetches w/ same label not replacing previous data

--- test/functional/heim/DataFetchingTaskTests.groovy
+++ test/functional/heim/DataFetchingTaskTests.groovy
@@ -14,28 +14,28 @@ class DataFetchingTaskTests extends BaseAPITestCase {
 
     private static final String DATA_FETCH_TASK_TYPE = DataFetchTaskFactory.FETCH_DATA_TASK_NAME
 
-    void testMrnaDataFetching() {
+    public static final LinkedHashMap<String, Serializable> TEST_ARGUMENTS = [
+            conceptKey      : '\\\\i2b2 main\\foo\\study1\\bar\\',
+            dataType        : 'mrna',
+            //resultInstanceId: 1, TODO: the test data has no result set for the mrna patients
+            assayConstraints: [
+                    trial_name: [name: MrnaTestData.TRIAL_NAME],
+            ],
+            dataConstraints : [
+                    genes: [names: ['BOGUSRQCD1']]
+            ],
+            projection      : 'zscore',
+            label           : 'test_label',
+    ]
 
-        def test_label = 'test_label'
+    void testMrnaDataFetching() {
         String sessionId = createSession('func_test')
 
         post '/ScriptExecution/run', {
             body json: [
                     sessionId: sessionId,
                     taskType: DATA_FETCH_TASK_TYPE,
-                    arguments: [
-                            conceptKey: '\\\\i2b2 main\\foo\\study1\\bar\\',
-                            dataType: 'mrna',
-                            //resultInstanceId: 1, TODO: the test data has no result set for the mrna patients
-                            assayConstraints: [
-                                    trial_name: [name: MrnaTestData.TRIAL_NAME],
-                            ],
-                            dataConstraints: [
-                                    genes: [ names: ['BOGUSRQCD1'] ]
-                            ],
-                            projection: 'zscore',
-                            label: test_label,
-                    ]
+                    arguments: TEST_ARGUMENTS,
             ]
         }
 
@@ -50,6 +50,35 @@ class DataFetchingTaskTests extends BaseAPITestCase {
 
         assertStatus 200
         assertThat JSON, hasEntry(is('state'), is(TaskState.FINISHED.toString()))
-        assertThat JSON.result.artifacts.currentLabels, contains(test_label)
+        assertThat JSON.result.artifacts.currentLabels, contains(TEST_ARGUMENTS.label)
+    }
+
+    void testRefetchingReplacesDataset() {
+        // instead of adding a new one
+        def test_label = 'test_label'
+        String sessionId = createSession('func_test')
+
+        2.times {
+            post '/ScriptExecution/run', {
+                body json: [
+                        sessionId: sessionId,
+                        taskType : DATA_FETCH_TASK_TYPE,
+                        arguments: TEST_ARGUMENTS,
+                ]
+            }
+
+            assertStatus 200
+
+            String taskId = JSON.executionId
+            get '/ScriptExecution/status?' + buildQueryParameters(
+                    sessionId: sessionId,
+                    executionId: taskId,
+                    waitForCompletion: true,
+            )
+
+            assertStatus 200
+            assertThat JSON, hasEntry(is('state'), is(TaskState.FINISHED.toString()))
+            assertThat JSON.result.artifacts.currentLabels, contains(TEST_ARGUMENTS.label)
+        }
     }
 }
