f6309e9260f7d050e356edbe83bf94425eceef06
Terry-Weymouth <weymouth@umich.edu>
2015-12-01 10:28:33 -0500

    Working version check for java

--- Sanity-check-ubuntu/checkVersions.sh
+++ Sanity-check-ubuntu/checkVersions.sh
@@ -7,13 +7,13 @@
 . ./versionCompare.sh
 
 javaVersion=$(java -version 2>&1 | awk -F '"' '{print $2}')
-echo "$javaVersion"
 javaVersion=$(echo $javaVersion | tr '_' '.')
-echo "$javaVersion"
-echo $(vercomp "1.7" $javaVersion)
-if [ $(vercomp "1.7" $javaVersion) -gt 0 ]; then
+$(vercomp "1.7" $javaVersion)
+versionTest=$?
+if [ "$versionTest" -gt 0 ]; then
     echo "Java version, $javaVersion, is good!"
 else 
     echo "Expected java verison 1.7 or better; currently $javaVersion; needs to be upgraded"
 fi
+
 
