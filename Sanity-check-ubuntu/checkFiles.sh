659a572f41295f0068b326620e7f013bda4a591b
Terry Weymouth <weymouth@umich.edu>
2015-12-02 14:40:57 -0500

    script to check for existance of required files and directories

---
+++ Sanity-check-ubuntu/checkFiles.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# ********************************************************************************
+# This script checks for and reports missing files and directories that are required
+# for the tranSAMRT install and data loading
+# ********************************************************************************
+
+base="$HOME/transmart/transmart-data"
+
+filepath="$base/vars"
+if [ ! -e "$filepath" ]; then
+	echo "The file at $filepath"
+	echo "  1s required and does not exist; this should have been created"
+	echo "  in the 'basics' step of the install; check the log and repeat that step"
+fi
+
+baseEnv="$base/env"
+for envDir in data-integration tranSMART-ETL
+do
+	filepath="$baseEnv/$envDir"
+	if [ ! -e "$filepath" ]; then
+		echo "The directory/folder at $filepath"
+		echo "  is required and does not exist; this should have been created"
+		echo "  in the 'buildEnv' step of the install; check the log and repeat that step"
+	fi
+done
+
+baseR="$base/R"
+filepath="$baseR/root/bin"
+if [ ! -e "$filepath" ]; then
+	echo "The file at $filepath"
+	echo "  1s required and does not exist; this should have been created"
+	echo "  in the 'buildR' step of the install; check the log and repeat that step"
+fi
+
