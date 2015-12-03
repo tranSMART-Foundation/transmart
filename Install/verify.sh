281fd7dbf540fe9765c2cb37a2b64492695bfa6c
Terry Weymouth <weymouth@umich.edu>
2015-12-03 15:59:39 -0500

    CLeaning up install directory

--- Install/verify.sh
+++
@@ -1,2 +0,0 @@
-type $1 >/dev/null 2>&1 || { echo >&2 "$1 is required, but it's not installed."; exit 1; }
-exit 0
--- Install/verifyTest.sh
+++
@@ -1,6 +0,0 @@
-if [ 0 = ( verify.sh ant ) ]; then
-    echo "ant ok"
-fi
-if [ 0 = { verify.sh foo } ]; then
-    echo "foo ok"
-fi
