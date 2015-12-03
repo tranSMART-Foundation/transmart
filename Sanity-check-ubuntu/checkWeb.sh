41c8a08068a0eeba9446c188a3c293075310b59c
Terry Weymouth <weymouth@umich.edu>
2015-12-03 15:00:32 -0500

    final version of checkWeb.sh

--- Sanity-check-ubuntu/checkWeb.sh
+++ Sanity-check-ubuntu/checkWeb.sh
@@ -8,6 +8,14 @@
 # # ------------------ source helper function -------------------
 . ./checkUrl.sh
 
+# ---------------------------
+# Check to see that the expected we sites are running
+# ---------------------------
+
+echo "-------------------------------------"
+echo "Check to see that the expected we sites are running"
+echo "-------------------------------------"
+
 solrUrl="http://localhost:8983/solr/#/"
 gwavaUrl="http://localhost:8080/gwava/"
 transmartUrl="http://localhost:8080/transmasrt"
@@ -21,6 +29,26 @@ gwavaResults=$?
 checkURL $transmartUrl
 transmartResults=$?
 
-echo "solr $solrResults"
-echo "gwava $gwavaResults"
-echo "transmart $transmartResults"
+exitResults=0
+if [ $solrResults -ne 0 ]; then
+	echo "SOLR (expected at $solrUrl) is not running; see detailed instructions to start it"
+	exitResults=1
+else
+	echo "SOLR ($solrUrl) is running"
+fi
+
+if [ $gwavaResults -ne 0 ]; then
+	echo "The GWAVA web site (expected at $gwavaUrl) is not running; see detailed instructions to start it"
+	exitResults=1
+else
+	echo "The GWAVA web site ($gwavaUrl) is running"
+fi
+
+if [ $transmartResults -ne 0 ]; then
+	echo "The tranSMART web site (expected at $transmartaUrl) is not running; see detailed instructions to start it"
+	exitResults=1
+else
+	echo "The tranSMART web site ($transmartaUrl) is running"
+fi
+
+exit $exitResults
