e1d04d528fffbdac4f3062e2d8c4b0d27770b515
Terry Weymouth <weymouth@umich.edu>
2015-12-03 14:20:04 -0500

    helper function to check for URL working

---
+++ Sanity-check-ubuntu/checkUrl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ********************************************************************************
+# This script tests for connection to and existence of a web site by URL
+# cribbed from: http://answers.google.com/answers/threadview/id/276934.html
+# ********************************************************************************
+
+function checkURL {
+    curl -s -o "/dev/null" $1
+    if [ $? -ne 0 ] ; then
+        echo "Error occurred getting URL $1:"
+        if [ $? -eq 6 ]; then
+            echo "  Unable to resolve host"
+        fi
+        if [$? -eq 7 ]; then
+            echo "  Unable to connect to host"
+        fi
+        return 1
+    fi
+    return 0;	
+}
