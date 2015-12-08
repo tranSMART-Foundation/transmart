1d14738024de241031b813e186369c49481c0e78
Terry-Weymouth <weymouth@umich.edu>
2015-12-08 16:51:09 -0500

    added check for tomcat install - corrected syntax in script

--- install-ubuntu/checks/checkTomcatInstall.sh
+++ install-ubuntu/checks/checkTomcatInstall.sh
@@ -8,23 +8,23 @@ echo "-------------------------------------"
 echo "|  Checking for tomcat install and modes to tomcat configuration"
 echo "-------------------------------------"
 
-if ! -e /etc/default/tomcat7 ; then
+if ! [ -e /etc/default/tomcat7 ] ; then
     echo "It appears that tomcat is not installed"
     echo "Please check install set and repeat test"
     exit 1
 fi
 
-sudo service tomcat7 status | grep "servlet engine"
+sudo service tomcat7 status | grep -q "servlet engine"
 results=$?
-if (! results) ; then
+if ! [ $results ] ; then
     echo "It appears that tomcat is not installed"
     echo "Please check install set and repeat test"
     exit 1
 fi
 
-grep "Xmx2g" /etc/default/tomcat7
+grep "Xmx2g" /etc/default/tomcat7 | grep -q "JAVA_OPTS"
 results=$?
-if (! results) ; then
+if ! [ $results ] ; then
     echo "It appears that the tomcat configuration for heap space"
     echo "was not modified; tomcat will not fun correctly"
     echo "the heap space in /etc/default/tomcat7 should be set to -Xmx2g"
