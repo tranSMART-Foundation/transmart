80667adacc177a6e489ede242c11a589a65d7d30
Piotr Zakrzewski <piotr@thehyve.nl>
2015-12-08 16:37:25 +0100

    Tighten input validation. Empty Bio.marker column not allowed.

--- web-app/HeimScripts/heatmap/preprocess.R
+++ web-app/HeimScripts/heatmap/preprocess.R
@@ -4,7 +4,7 @@ library("WGCNA")
 main <- function(aggregate=FALSE){
   msgs = c("")
   df <- mergeFetchedData(loaded_variables)
-  good.input <- ncol(df) > 3 && nrow(df) > 1
+  good.input <- ncol(df) > 3 && nrow(df) > 0 && sum(df$Bio.marker != "") > 0 # more than one sample, contains any rows, non empty Bio.marker column.
   if(aggregate && good.input){
     df <- dropEmptyGene(df)
     aggr  <- aggregate.probes(df)
