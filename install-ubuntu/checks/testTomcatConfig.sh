9769bbdb722dd28c065aba3180fb04ec77cb8304
Terry Weymouth <weymouth@umich.edu>
2015-12-08 15:41:33 -0500

    Replaced testTomcatConfig.sh
    Copy past text from working version

--- install-ubuntu/checks/testTomcatConfig.sh
+++ install-ubuntu/checks/testTomcatConfig.sh
@@ -1,9 +1,12 @@
-sudo cp /etc/default/tomcat7 /etc/default/tomcat7-backup-$(Date +%s)
 # edit /etc/default/tomcat7 replace with '-Xmx128m' with '-Xms512m -Xmx2g'
-# in JAVA_OPTS="-Djava.awt.headless=true -Xms512m -Xmx2g -XX:+UseConcMarkSweepGC"
+# in JAVA_OPTS="-Djava.awt.headless=true -Xms512m -Xmx2g -XX:+UseConcMarkSweepGC
+"
 # unless it is already there
-tempFileName="tempFile$(Date +%s).txt"
 if ! (grep "mx2g" /etc/default/tomcat7); then
-	sed "s/-Xmx128m/-Xms512m -Xmx2g/g" "/etc/default/tomcat7" > $tempFileName
-	sudo mv $tmpFileName /etc/default/tomcat7
-if
+    sudo cp /etc/default/tomcat7 /etc/default/tomcat7-backup-$(date +%s)
+    tempFileName="tempFile$(date +%s).txt"
+    sed "s/-Xmx128m/-Xms512m -Xmx2g/g" "/etc/default/tomcat7" > "$tempFileName"
+    sudo rm "/etc/default/tomcat7"
+    sudo cp "$tempFileName" "/etc/default/tomcat7"
+    rm $tempFileName
+fi
