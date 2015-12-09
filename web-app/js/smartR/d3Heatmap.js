34b066e9390e5215ffd70b877d003cc89dc1e31b
Piotr Zakrzewski <piotr@thehyve.nl>
2015-12-09 16:36:24 +0100

    :fish:

--- web-app/js/smartR/d3Heatmap.js
+++ web-app/js/smartR/d3Heatmap.js
@@ -136,8 +136,7 @@ SmartRHeatmap = (function(){
                 .attr('class', function(d) {
                     return 'square patientID-' + d.PATIENTID + ' probe-' + d.PROBE;
                 })
-                .attr("x", function(d) {
-                    return patientIDs.indexOf(d.PATIENTID) * gridFieldWidth; })
+                .attr("x", function(d) { return patientIDs.indexOf(d.PATIENTID) * gridFieldWidth; })
                 .attr("y", function(d) { return probes.indexOf(d.PROBE) * gridFieldHeight; })
                 .attr("width", gridFieldWidth)
                 .attr("height", gridFieldHeight)
@@ -172,8 +171,7 @@ SmartRHeatmap = (function(){
             square
                 .transition()
                 .duration(animationDuration)
-                .attr("x", function(d) {
-                    return patientIDs.indexOf(d.PATIENTID) * gridFieldWidth; })
+                .attr("x", function(d) { return patientIDs.indexOf(d.PATIENTID) * gridFieldWidth; })
                 .attr("y", function(d) { return probes.indexOf(d.PROBE) * gridFieldHeight; })
                 .attr("width", gridFieldWidth)
                 .attr("height", gridFieldHeight);
@@ -485,7 +483,7 @@ SmartRHeatmap = (function(){
                 .attr('class', function(d) {
                     return 'extraSquare patientID-' + d.PATIENTID + ' feature-' + d.FEATURE;
                 })
-                .attr("x", function(d) {return patientIDs.indexOf(d.PATIENTID) * gridFieldWidth; })
+                .attr("x", function(d) { return patientIDs.indexOf(d.PATIENTID) * gridFieldWidth; })
                 .attr('y', function(d) { return featurePosY + features.indexOf(d.FEATURE) * gridFieldHeight / 2; })
                 .attr("width", gridFieldWidth)
                 .attr("height", gridFieldHeight / 2)
@@ -905,14 +903,11 @@ SmartRHeatmap = (function(){
         }
 
         function updateColOrder(sortValues) {
-            console.log(sortValues);
             var sortedPatientIDs = [];
             for (var i = 0; i < sortValues.length; i++) {
                 sortedPatientIDs.push(patientIDs[sortValues[i]]);
             }
-            console.log(patientIDs);
             patientIDs = sortedPatientIDs;
-            console.log(patientIDs);
             unselectAll();
             removeColDendrogram();
             updateHeatmap();
