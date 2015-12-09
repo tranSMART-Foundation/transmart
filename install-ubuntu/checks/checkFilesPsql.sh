f5ff930cb101ebbb4820abda4e0422855940a2d3
Terry Weymouth <weymouth@umich.edu>
2015-12-09 14:38:00 -0500

    Initial pass on check tablespace files for posgreSQL

--- install-ubuntu/checks/checkFilesPsql.sh
+++ install-ubuntu/checks/checkFilesPsql.sh
@@ -4,30 +4,26 @@
 # This script checks for and reports missing files and directories that are required
 # for the tranSAMRT install and data loading
 # ********************************************************************************
+. ./postgresCheckHelper.sh
 
 echo "-------------------------------------"
-echo "|  Checking for required end point files and folders"
+echo "|  Checking for postgres support folders"
 echo "-------------------------------------"
-
-base="$HOME/transmart/transmart-data"
-baseEnv="$base/env"
-baseR="$base/R"
-baseConfig="/usr/share/tomcat7/.grails/transmartConfig"
-baseWebapps="/var/lib/tomcat7/webapps"
-baseLogs="/var/lib/tomcat7/logs"
 
 returnValue=0
 
-filepath="$baseR/root/bin"
-if [ ! -e "$filepath" ]; then
-	echo "The file at $filepath"
-	echo "  is required and does not exist; this should have been created"
-	echo "  in the 'buildR' step of the install; repeat that step"
-	returnValue=1
-fi
+for folderName in biomart deapp indx search_app transmart
+do
+	if ! checkForPostgresTablespaceFolder $folderName; then
+		echo "The required postgres tablespace folder $folderName"
+		echo "  is missing; this should have been created as part of the basics"
+		echo "  stage in the install process; recheck that step"
+		returnValue=1
+	fi
+done
 
 if [ 0 = $returnValue ] ; then
-	echo "All required end point files and folders are present"
+	echo "All required postgres support folders are present"
 fi
 
 exit $returnValue
