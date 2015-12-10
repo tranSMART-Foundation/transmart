fbb83772cebfa31ca91cf48dfe1ecb93b87fd5a1
Terry Weymouth <weymouth@umich.edu>
2015-12-10 08:24:18 -0500

    removing old versions of install script

--- install-ubuntu/install02_TransmartDataBasics.sh
+++
@@ -1,8 +0,0 @@
-cd $HOME/transmart/
-curl https://codeload.github.com/tranSMART-Foundation/transmart-data/zip/release-1.2.4 -o transmart-data-release-1.2.4.zip
-unzip transmart-data-release-1.2.4.zip
-mv transmart-data-release-1.2.4 transmart-data
-cd $HOME/transmart/transmart-data
-sudo make -C env ubuntu_deps_root
-make -C env ubuntu_deps_regular
-rm $HOME/transmart/transmart-data/env/groovy
--- install-ubuntu/install03_RAndRserve.sh
+++
@@ -1,6 +0,0 @@
-cd $HOME/transmart/transmart-data
-. ./vars
-make -C R install_packages
-echo "export PATH=${HOME}/transmart/transmart-data/R/root/bin:$PATH" > Rpath.sh
-sudo mv Rpath.sh /etc/profile.d/
-source /etc/profile.d/Rpath.sh
