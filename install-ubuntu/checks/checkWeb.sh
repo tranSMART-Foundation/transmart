cc4f311ca411b862d8c31de2a5f335059483e029
Terry Weymouth <weymouth@umich.edu>
2015-12-10 13:30:37 -0500

    Corrected syntax error in checkWeb.sh

--- install-ubuntu/checks/checkWeb.sh
+++ install-ubuntu/checks/checkWeb.sh
@@ -64,7 +64,8 @@ else
 	echo "The tranSMART web site ($transmartUrl) is running"
 fi
 
-if ! $(curl $transmartUrl | grep "title" | grep "tranSMART") ; then
+probe=$(curl $transmartUrl | grep "title" | grep "tranSMART")
+if [ -z "$probe" ] ; then
 	echo "The tranSMART web site (at $transmartUrl) is not delivering the login home page;"
 	echo "  see tomcat log file, transmart.log for possible errors "
 	exitResults=1
