cfe49ce1f9f0ca1ae761168c7f433d8f827ab964
Terry Weymouth <weymouth@umich.edu>
2015-12-22 14:59:52 -0500

    update package list for test after make; dropped ant mvn which are loaded later

--- install-ubuntu/checks/checkMakefilePackages.sh
+++ install-ubuntu/checks/checkMakefilePackages.sh
@@ -9,13 +9,14 @@
 . ./basicsHelper.sh
 
 echo "-------------------------------------"
-echo "|  Checking for basic command-line tools; if any of the following does not exist,"
+echo "|  Checking for basic command-line tools; that are installed by make"
+echo "|  if any of the following does not exist,"
 echo "|  then recheck the instructions for installing the missing items; "
 echo "|  also ref: installTransmart.sh,'Install of basic tools' "
 echo "-------------------------------------"
 
 probe=0
-for command in git make java ant mvn tar rsync php g++ gfortran
+for command in git make java tar rsync php g++ gfortran
 do
     if ! checkForCommandLineTool "$command"; then
         probe=1
