220c3dbbe270d1175e72fc8584c2a157e6a8713a
Terry Weymouth <weymouth@umich.edu>
2015-12-24 12:56:54 -0500

    added -qq option to apt-get commands; no list except errors

--- install-ubuntu/runInstall.sh
+++ install-ubuntu/runInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y git
+sudo apt-get -qq update
+sudo apt-get -qq install -y git
 
 # The location of the install directories
 INSTALL_BASE="$HOME/transmart"
@@ -13,3 +13,4 @@ export INSTALL_BASE SCRIPTS_BASE
 cd "$SCRIPTS_BASE"
 git clone https://github.com/tranSMART-Foundation/Scripts.git
 Scripts/install-ubuntu/InstallTransmart.sh 2>&1 | tee install.log
+
