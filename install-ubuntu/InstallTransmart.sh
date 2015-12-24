e1c5f06d1c67e720c612a3aa5691360bdf06c5f4
Terry Weymouth <weymouth@umich.edu>
2015-12-24 10:20:30 -0500

    Added BASE directories for the scripts and the install

--- install-ubuntu/InstallTransmart.sh
+++ install-ubuntu/InstallTransmart.sh
@@ -15,7 +15,7 @@
 #   cd ~
 #   /Scripts/install-ubuntu/InstallTransmart.sh
 #
-# to run the checking script
+# to run the checking scripts
 #   Scripts/install-ubuntu/checks/checkAll.sh
 
 # on error; stop/exit
@@ -33,18 +33,35 @@ function checkInstallError {
 	return $returnValue
 }
 
+if [ -z "$SCRIPTS_BASE" ] ; then SCRIPTS_BASE="$HOME" ; fi
+
 echo "Starting at $(date)"
 echo "++++++++++++++++++++++++++++"
 echo "+  Checking locations of Script Directory"
 echo "++++++++++++++++++++++++++++"
-if ! [ -e $HOME/Scripts ] ; then
-	echo "This script assumes that the Scripts directory is installed at $HOME/Scripts"
+if ! [ -d "$SCRIPTS_BASE/Scripts" ] ; then
+	echo "This script assumes that the Scripts directory is installed at $SCRIPTS_BASE/Scripts"
 	echo "It does not appear to be there. Please fix that and restart this script."
+	echo "  cd $SCRIPTS_BASE"
+	echo "  sudo apt-get update"
+	echo "  sudo apt-get install -y git"
+	echo "  git clone https://github.com/tranSMART-Foundation/Scripts.git"
 	exit 1
 else
-	echo "Script directory found: $HOME/Scripts"
+	echo "Script directory found: $SCRIPTS_BASE/Scripts"
 fi
 echo "Finished checking locations of Script Directory at $(date)"
+
+echo "++++++++++++++++++++++++++++"
+echo "+  set up working dir (tranSMAPT install base) "
+echo "++++++++++++++++++++++++++++"
+
+if [ -z "$INSTALL_BASE" ] ; then INSTALL_BASE="$HOME/transmart" ; fi
+
+if ! [ -d $HOME/transmart ] ; then
+	mkdir -p "$INSTALL_BASE"
+fi
+echo "tranSMART will be installed at this location: $INSTALL_BASE"
 
 cd $HOME/Scripts/install-ubuntu
 source welcome.sh
@@ -59,18 +76,10 @@ echo "++++++++++++++++++++++++++++"
 sudo apt-get install -y make
 
 echo "++++++++++++++++++++++++++++"
-echo "+  set up working dir (transmart) "
-echo "++++++++++++++++++++++++++++"
-
-if ! [ -e $HOME/transmart ] ; then
-	mkdir $HOME/transmart
-fi
-
-echo "++++++++++++++++++++++++++++"
 echo "+  set up the transmart-date folder"
 echo "++++++++++++++++++++++++++++"
 
-cd $HOME/transmart
+cd $INSTALL_BASE
 sudo -v
 sudo apt-get install -y curl
 sudo apt-get install -y unzip
@@ -93,20 +102,20 @@ echo "++++++++++++++++++++++++++++"
 #   make of these two steps: 
 # (1)
 sudo -v
-cd $HOME/transmart/transmart-data
+cd $INSTALL_BASE/transmart-data
 sudo make -C env install_ubuntu_packages 
 # verify these packages
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkMakefilePackages.sh
 if [ "$( checkInstallError "Some Basic Command-Line Tool from 'sudo make -C env install_ubuntu_packages' is missing; redo install" )" ] ; then exit -1; fi
 echo "sudo make -C env install_ubuntu_packages - finished at $(date)"
 
 # (2)
 sudo -v
-cd $HOME/transmart/transmart-data
+cd $INSTALL_BASE/transmart-data
 sudo make -C env /var/lib/postgresql/tablespaces
 # verify tablespaces
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkFilesTablespace.sh
 if [ "$( checkInstallError "TABLESPACE files (/var/lib/postgresql/tablespaces) not set up properly; redo install" )" ] ; then exit -1; fi
 echo "sudo make -C env /var/lib/postgresql/tablespaces - finished at $(date)"
@@ -119,30 +128,30 @@ echo "Finished setting ubuntu dependencies (with root) at $(date)"
 #   make of these four steps: 
 # (1)
 sudo -v
-cd $HOME/transmart/transmart-data
+cd $INSTALL_BASE/transmart-data
 make -C env update_etl
 # verify ETL folder
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkFilesETLFolder.sh
 if [ "$( checkInstallError "The directory transmart-data/tranSMART-ETL was not installed properly; redo install" )" ] ; then exit -1; fi
 echo "make -C env update_etl - finished at $(date)"
 
 # (2)
 sudo -v
-cd $HOME/transmart/transmart-data
+cd $INSTALL_BASE/transmart-data
 make -C env data-integration 
 # verify data-integration folder
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkFilesDataIntegrationFolder.sh
 if [ "$( checkInstallError "The directory transmart-data/data-integration was not installed properly; redo install" )" ] ; then exit -1; fi
 echo "make -C env data-integration - finished at $(date)"
 
 # (3)
 sudo -v
-cd $HOME/transmart/transmart-data
+cd $INSTALL_BASE/transmart-data
 make -C env ../vars
 # verify setup of vars file
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkFilesVars.sh
 if [ "$( checkInstallError "vars file (transmart-data/vars) not set up properly; redo install" )" ] ; then exit -1; fi
 echo "make -C env ../vars - finished at $(date)"
@@ -157,15 +166,15 @@ sudo apt-get install -y ant
 sudo apt-get install -y maven
 echo "Finished install of ant and maven at $(date)"
 
-# No longer to remove this as the make step that creates it is not skipped!
-# rm $HOME/transmart/transmart-data/env/groovy
+# No longer need to remove this as the make step that creates it is not skipped!
+# rm $INSTALL_BASE/transmart-data/env/groovy
 
 curl -s get.sdkman.io | bash
 echo "Y" > AnswerYes.txt
 source $HOME/.sdkman/bin/sdkman-init.sh
 sdk install grails 2.3.11 < AnswerYes.txt
 sdk install groovy 2.4.5 < AnswerYes.txt
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkSdkmanApps.sh
 if [ "$( checkInstallError "groovy and/or grails not installed correctly; redo install" )" ] ; then exit -1; fi
 echo "Finished install of groovy and grails at $(date)"
@@ -173,12 +182,12 @@ echo "Finished install of groovy and grails at $(date)"
 # fix files for postgres
 echo "Patch dir permits for TABLESPACES"
 sudo -v
-cd $HOME/transmart/transmart-data
+cd $INSTALL_BASE/transmart-data
 . ./vars
 sudo chmod 700 $TABLESPACES/*
 
 echo "Checks on basic load"
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./basics.sh
 if [ "$( checkInstallError "Some Basic Command-Line Tool is missing; redo install" )" ] ; then exit -1; fi
 ./checkVersions.sh
@@ -202,10 +211,10 @@ sudo -v
 cd $HOME
 sudo apt-get install -y tomcat7 
 sudo service tomcat7 stop
-$HOME/Scripts/install-ubuntu/updateTomcatConfig.sh
+$SCRIPTS_BASE/Scripts/install-ubuntu/updateTomcatConfig.sh
 
 echo "+  Checks on tomcat install"
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkTomcatInstall.sh
 if [ "$( checkInstallError "Tomcat install failed; redo install" )" ] ; then exit -1; fi
 
@@ -221,14 +230,14 @@ echo "++++++++++++++++++++++++++++"
 
 #sudo -v
 #sudo apt-get install -y r-base=3.0.2-1ubuntu1
-#cd $HOME/transmart/transmart-data/R
+#cd $INSTALL_BASE/transmart-data/R
 #R_MIRROR="http://cran.utstat.utoronto.ca/"
 #R_EXEC=$(which R)
 #sudo CRAN_MIRROR=$(R_MIRROR) $(R_EXEC) -f cran_pkg.R
 #sudo CRAN_MIRROR=$(R_MIRROR) $(R_EXEC) -f other_pkg.R
 # also set up install_rserve_init (see below)
 
-base="$HOME/transmart/transmart-data"
+base="$INSTALL_BASE/transmart-data"
 baseR="$base/R"
 filepath="$baseR/root/bin"
 if [ -e "$filepath" ]; then
@@ -236,7 +245,7 @@ if [ -e "$filepath" ]; then
 else
 	echo "+  installing R at $filepath"
     sudo -v
-    cd $HOME/transmart/transmart-data
+    cd $INSTALL_BASE/transmart-data
     source ./vars
     make -C R install_packages
 fi
@@ -249,11 +258,11 @@ fi
 source /etc/profile.d/Rpath.sh
 
 sudo -v
-cd $HOME/transmart/transmart-data
+cd $INSTALL_BASE/transmart-data
 source ./vars
 sudo TABLESPACES=$TABLESPACES TRANSMART_USER="tomcat7" make -C R install_rserve_init
 
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkFilesR.sh
 if [ "$( checkInstallError "R install failed; redo install" )" ] ; then exit -1; fi
 ./checkR.sh
@@ -264,7 +273,7 @@ echo "++++++++++++++++++++++++++++"
 echo "+  Load study GSE8581 in database"
 echo "++++++++++++++++++++++++++++"
 
-cd $HOME/transmart/transmart-data
+cd $INSTALL_BASE/transmart-data
 source ./vars
 make -j4 postgres
 echo "Finished setting up the PostgreSQL database at $(date)"
@@ -273,7 +282,7 @@ make -C samples/postgres load_clinical_GSE8581
 make -C samples/postgres load_ref_annotation_GSE8581
 make -C samples/postgres load_expression_GSE8581
 
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkPsqlDataLoad.sh
 if [ "$( checkInstallError "Loading database failed; clear database and restart install" )" ] ; then exit -1; fi
 
@@ -283,14 +292,14 @@ echo "++++++++++++++++++++++++++++"
 echo "+  Set up configuration files"
 echo "++++++++++++++++++++++++++++"
 
-cd $HOME/transmart/transmart-data
+cd $INSTALL_BASE/transmart-data
 sudo -v
 source ./vars
 make -C config install
 sudo mkdir -p /usr/share/tomcat7/.grails/transmartConfig/
 sudo cp $HOME/.grails/transmartConfig/*.groovy /usr/share/tomcat7/.grails/transmartConfig/
 
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkFilesConfig.sh
 if [ "$( checkInstallError "configuration files not set up correctly, see install script and redo" )" ] ; then exit -1; fi
 
@@ -300,7 +309,7 @@ echo "++++++++++++++++++++++++++++"
 echo "+  Install war files"
 echo "++++++++++++++++++++++++++++"
 
-cd $HOME/transmart
+cd $INSTALL_BASE
 sudo -v
 mkdir war-files
 cd war-files
@@ -308,7 +317,7 @@ curl http://75.124.74.64/wars/transmart.V1.2.4.war --output transmart.war
 curl http://75.124.74.64/wars/gwava.V1.2.4.war --output gwava.war
 sudo cp *.war /var/lib/tomcat7/webapps/
 
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkFilesTomcatWar.sh
 if [ "$( checkInstallError "transmart war file not set up correctly, see install script and redo" )" ] ; then exit -1; fi
 
@@ -318,10 +327,10 @@ echo "++++++++++++++++++++++++++++"
 echo "+  Load, configure and start SOLR"
 echo "++++++++++++++++++++++++++++"
 
-cd $HOME/transmart/transmart-data
+cd $INSTALL_BASE/transmart-data
 sudo -v
 source ./vars
-make -C solr start > $HOME/transmart/transmart-data/solr.log 2>&1 & 
+make -C solr start > $INSTALL_BASE/transmart-data/solr.log 2>&1 & 
 sleep 60
 make -C solr rwg_full_import sample_full_import
 echo "Finished loading, configuring and starting SOLR at $(date)"
@@ -331,7 +340,7 @@ echo "+  start Rserve"
 echo "++++++++++++++++++++++++++++"
 
 sudo -v
-cd $HOME/Scripts/install-ubuntu
+cd $SCRIPTS_BASE/Scripts/install-ubuntu
 sudo -u tomcat7 bash ./runRServe.sh
 #sudo service rserve start - is not working - not sure why
 echo "Finished starting RServe at $(date)"
@@ -347,14 +356,14 @@ echo "++++++++++++++++++++++++++++"
 echo "+ Done with install - making final checks (may take a while)"
 echo "++++++++++++++++++++++++++++"
 
-cd $HOME/Scripts/install-ubuntu/checks
+cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks
 ./checkFilesTomcat.sh
 ./checkTools.sh
 ./checkWeb.sh
 
 echo "++++++++++++++++++++++++++++"
 echo "+ To redo all checks"
-echo "cd ~/Scripts/install-ubuntu/checks/"
+echo "cd $SCRIPTS_BASE/Scripts/install-ubuntu/checks"
 echo "./checkAll.sh"
 echo "++++++++++++++++++++++++++++"
 echo "+ Done with final checks"
