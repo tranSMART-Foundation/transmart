5e3d29ae9c2a741d04bdf69b24482d01f29ab314
Terry Weymouth <weymouth@umich.edu>
2015-12-24 13:12:20 -0500

    added silent mode to checkPsqlDataLoad.sh

--- install-ubuntu/checks/checkPsqlDataLoad.sh
+++ install-ubuntu/checks/checkPsqlDataLoad.sh
@@ -7,64 +7,97 @@
 # are the admin and guest accounts established; has the GSP8581 database been loaded
 # ******************************************************************************
 
-# # ------------------ helper function -------------------
+# special case with no echo
+runWithEcho=1
+if [ -z $1 ] ; then 
+	runWithEcho=0
+fi
 
-echo "-------------------------------------"
-echo "|  Checking for transmart database access: "
-echo "|    does 'biomart' use exist "
-echo "|    are the transmart users set up: admin and guest  "
-echo "|    in the demo dataset GSE8581 loaded  "
-echo "-------------------------------------"
+if ($runWithEcho) ; then
+	echo "-------------------------------------"
+	echo "|  Checking for transmart database access: "
+	echo "|    does 'biomart' use exist "
+	echo "|    are the transmart users set up: admin and guest  "
+	echo "|    in the demo dataset GSE8581 loaded  "
+	echo "-------------------------------------"
+if
 
-echo "Checking SUDO authentication"
-sudo echo "Established SUDO authentication"
+sudo -v
+if ($runWithEcho) ; then
+	echo "Checking SUDO authentication"
+	sudo echo "Established SUDO authentication"
+fi
 
-echo "checking to see if PostgreSQL is running"
+if ($runWithEcho) ; then
+	echo "checking to see if PostgreSQL is running"
+fi
 postgresRunning=$(ps aux | grep postgres | grep -v "grep" | grep "stats collector process")
 if [ -z "$postgresRunning" ]; then 
-	echo "PostgreSQL does not appear to be running; start it"
-	echo  "  with the command: sudo /etc/init.d/postgresql restart"  
+	if ($runWithEcho) ; then
+		echo "PostgreSQL does not appear to be running; start it"
+		echo  "  with the command: sudo /etc/init.d/postgresql restart"  
+	fi
 	exit 1
 fi
-echo "PostgreSQL appears to be running"
+if ($runWithEcho) ; then
+	echo "PostgreSQL appears to be running"
+if
 
 results=$(sudo -u postgres psql postgres --command="\du biomart" | grep biomart)
 if [ -z "$results" ]; then
-	echo "The transmart database user 'biomart' does not exist;"
-	echo "  it is likely that you did not initialisze the trasnamrt database."
-	echo "  See install instructions to do so."
+	if ($runWithEcho) ; then
+		echo "The transmart database user 'biomart' does not exist;"
+		echo "  it is likely that you did not initialisze the trasnamrt database."
+		echo "  See install instructions to do so."
+	fi
 	exit 1
 fi 
-echo "The transmart database user 'biomart' exists."
+if ($runWithEcho) ; then
+	echo "The transmart database user 'biomart' exists."
+fi
 
 results=$(sudo -u postgres psql transmart --command="select username from searchapp.search_auth_user where username='admin'" | grep admin)
 if [ -z "$results" ]; then
-	echo "The transmartApp user 'admin' does not exist;"
-	echo "  it is likely that you did not initialisze the trasnamrt database."
-	echo "  See install instructions to do so."
+	if ($runWithEcho) ; then
+		echo "The transmartApp user 'admin' does not exist;"
+		echo "  it is likely that you did not initialisze the trasnamrt database."
+		echo "  See install instructions to do so."
+	fi
 	exit 1
 fi 
-echo "The transmartApp user 'admin' exists."
+if ($runWithEcho) ; then
+	echo "The transmartApp user 'admin' exists."
+fi
 
 results=$(sudo -u postgres psql transmart --command="select username from searchapp.search_auth_user where username='guest'" | grep guest)
 if [ -z "$results" ]; then
-	echo "The transmartApp user 'guest' does not exist;"
-	echo "  it is likely that you did not initialisze the trasnamrt database."
-	echo "  See install instructions to do so."
+	if ($runWithEcho) ; then
+		echo "The transmartApp user 'guest' does not exist;"
+		echo "  it is likely that you did not initialisze the trasnamrt database."
+		echo "  See install instructions to do so."
+	fi
 	exit 1
 fi 
-echo "The transmartApp user 'guest' exists."
+if ($runWithEcho) ; then
+	echo "The transmartApp user 'guest' exists."
+fi
 
 results=$(sudo -u postgres psql transmart --command="select concept_path from i2b2demodata.concept_dimension where concept_path like '%GSE8581\\\\'" | grep "GSE8581")
 if [ -z "$results" ]; then
-	echo "The transmartApp demo dataset GSE8581 has not been loaded"
-	echo "  it is likely that you did not load this dataset into the trasnamrt database."
-	echo "  See install instructions to do so."
+	if ($runWithEcho) ; then
+		echo "The transmartApp demo dataset GSE8581 has not been loaded"
+		echo "  it is likely that you did not load this dataset into the trasnamrt database."
+		echo "  See install instructions to do so."
+	fi
 	exit 1
 fi 
-echo "The transmartApp demo dataset GSE8581 is loaded."
+if ($runWithEcho) ; then
+	echo "The transmartApp demo dataset GSE8581 is loaded."
+fi
 
-echo "Done checking the PostgreSQL transmart database"
+if ($runWithEcho) ; then
+	echo "Done checking the PostgreSQL transmart database"
+fi
 
 exit 0
 
