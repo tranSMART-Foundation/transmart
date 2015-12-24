6e8d8fefddaa2040cbbc4642f6def3ec64f169e7
Terry Weymouth <weymouth@umich.edu>
2015-12-24 14:18:21 -0500

    added check for INSTALL_BASE to runRServe.sh

--- install-ubuntu/runRServe.sh
+++ install-ubuntu/runRServe.sh
@@ -1,4 +1,9 @@
-cd $HOME/transmart/transmart-data
+# INSTALL_BASE needs to be set; see InstallTransmart.sh
+if [ -z "$INSTALL_BASE" ] ; then
+	return;
+fi
+
+cd $INSTALL_BASE/transmart-data
 source vars
 source /etc/profile.d/Rpath.sh
 make -C R start_Rserve &
