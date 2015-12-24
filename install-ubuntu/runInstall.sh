8bbaac9d1e0f5f69f3983f7438bbdecbd17d6606
Terry Weymouth <weymouth@umich.edu>
2015-12-24 12:28:39 -0500

    Updated runInstall.sh to illustrate use of INSTALL_BASE and SCRIPTS_BASE

--- install-ubuntu/runInstall.sh
+++ install-ubuntu/runInstall.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-cd $HOME
 sudo apt-get update
 sudo apt-get install -y git
+
+INSTALL_BASE="$HOME/transmart"
+SCRIPTS_BASE="$INSTALL_BASE"
+
+export INSTALL_BASE SCRIPTS_BASE
+
+cd "$SCRIPTS_BASE"
 git clone https://github.com/tranSMART-Foundation/Scripts.git
 Scripts/install-ubuntu/InstallTransmart.sh 2>&1 | tee install.log
