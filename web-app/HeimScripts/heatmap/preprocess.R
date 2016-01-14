5fa51db33bcc021b389e7c5ee878f802224a4203
Rianne Jansen <rianne@thehyve.nl>
2016-01-14 13:52:15 +0100

    fix error when applying preprocessing on probe aggregation=Yes for 1 biomarker

--- web-app/HeimScripts/heatmap/preprocess.R
+++ web-app/HeimScripts/heatmap/preprocess.R
@@ -28,6 +28,12 @@ main <- function(aggregate=FALSE){
 }
 
 aggregate.probes <- function(df){
+  if(nrow(df)<2){
+    stop("Cannot aggregate probes: there only is data for a single probe (ie. only one row of data) or 
+        there is insufficient bio.marker information for the selected probes to be able to match the probes to 
+        biomarkers for aggregation (e.g. in case of micro-array data to match probe ID to gene symbol). 
+        Suggestion: skip probe aggregation.")
+  }
   measurements <- df[,3:ncol(df)]
   row.names(measurements) <- df[,1]
   collapsed <- collapseRows(measurements, df[,2], df[,1], "MaxMean",
@@ -35,6 +41,17 @@ aggregate.probes <- function(df){
                             methodFunction = NULL, # It only needs to be specified if method="function"
                             #connectivityPower = 1, # ignored when connectivityBasedCollapsing = FALSE
                             selectFewestMissing = FALSE)
+  
+  #collapsed returns 0 if collapseRows does not work, otherwise it returns a list
+  if(is.numeric(collapsed))
+  {
+    stop("Probe aggregation is not possible: there are too many missing data points in the data for succesfull probe
+aggregation. Skip probe aggregation or select more genes/proteins/metabolites/... 
+
+Note: for aggregation only the data from probes that have accompanying bio.marker information is used
+(e.g. in case of micro-array data only the probes are used that have gene symbol information coupled to it).
+This could mean the dataset used for aggregation is smaller than the initially selected dataset.")
+  }
   collapsedMeasurements <- collapsed$datETcollapsed
   Bio.marker <- collapsed$group2row[,1] # first column of this matrix always contains gene
   Row.Label <- collapsed$group2row[,2]  # second column of this matrix always contains probe_id
@@ -44,7 +61,8 @@ aggregate.probes <- function(df){
   df["Bio.marker"] <- Bio.marker
   df["Row.Label"] <- Row.Label
   row.names(df) <- NULL # WGCNA adds row.names. We do not need them to be set
-  df[,c(lastColIndex, lastbutOne , 1:(lastbutOne-1))]
+  return(df[,c(lastColIndex, lastbutOne , 1:(lastbutOne-1))])
+ 
 }
 
 dropEmptyGene <- function(d){
