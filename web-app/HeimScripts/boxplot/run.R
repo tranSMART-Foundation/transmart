abde060a2ebbbf03f8ec00121e7b87a69cb3d426
Piotr Zakrzewski <piotr@thehyve.nl>
2016-01-20 16:38:11 +0100

    For Single concept single node returns JSON compatibile with 0.4 version of vanilla SmartR as a boxplot.json file

--- web-app/HeimScripts/boxplot/run.R
+++ web-app/HeimScripts/boxplot/run.R
@@ -1,30 +1,50 @@
 
+
 main <- function(excludedPatientIDs) {
-	data <- list()
-	data$cohort1 <- list()
-	data$cohort2 <- list()
-	data$cohort1$concept <- names(loaded_variables)[1]  # tell the subsets apart by _sX identifier
-	subset.points <- loaded_variables[0]
-	bxp <- boxplot(subset.points$value, plot=FALSE)
-	subset <- "no subset"
-    nonEmptySubsets <- c(nonEmptySubsets, subset)
-    sorting <- match(subset.points$patientID, subset.values$patientID)
-    subset.points <- subset.points[order(sorting), ]
-    subset.points <- cbind(subset.points, subset.values$value)
-    jitter <- runif(nrow(subset.points), -0.5, 0.5)
-    subset.points <- cbind(subset.points, jitter)
-    names(subset.points) <- c('patientID', 'subset', 'value', 'jitter')
-    assignedPatientIDs <- c(assignedPatientIDs, subset.points$patientID)
-    bxp <- boxplot(subset.points$value, plot=FALSE)
-    data[[subset]] <- list()
-    data[[subset]]$lowerWhisker <- bxp$stats[1]
-    data[[subset]]$lowerHinge <- bxp$stats[2]
-    data[[subset]]$median <- bxp$stats[3]
-    data[[subset]]$upperHinge <- bxp$stats[4]
-    data[[subset]]$upperWhisker <- bxp$stats[5]
-    outlier <- subset.points$value > data[[subset]]$upperWhisker | subset.points$value < data[[subset]]$lowerWhisker
-    subset.points <- cbind(subset.points, outlier)
-    data[[subset]]$points <- subset.points
-	jsn <- toJson(data, pretty=TRUE)
-	print(jsn)
+  datapoints <- loaded_variables[[1]]
+  data <- list()
+  points <- datapoints[c('patientID', 'value')]
+  subsets <- list() 
+  patientIDs <- points$patientID
+  concept <- names(loaded_variables)[1]  #  datapoints$concept[1]
+  data$concept <- concept
+  data$globalMin <- min(points$value)
+  data$globalMax <- max(points$value)
+  assignedPatientIDs <- c()
+  nonEmptySubsets <- c()
+  for (subset in c(unique(subsets$value), 'no subset')) {
+    if (subset != 'no subset') {
+      subset.points <- subsets[subsets$value == subset & subsets$patientID %in% patientIDs, ]
+      subset.values <- points[points$patientID %in% subset.points$patientID, ]
+    } else {
+      unassignedPatientIDs <- patientIDs[! patientIDs %in% assignedPatientIDs]
+      subset.points <- data.frame(patientID=unassignedPatientIDs, subset=rep(NA, length(unassignedPatientIDs)))			
+      subset.values <- points[points$patientID %in% unassignedPatientIDs, ]
+    }
+    subset.points <- subset.points[! subset.points$patientID %in% excludedPatientIDs, ]
+    subset.values <- subset.values[! subset.values$patientID %in% excludedPatientIDs, ]
+    if (nrow(subset.points) > 0) {
+      nonEmptySubsets <- c(nonEmptySubsets, subset)
+      sorting <- match(subset.points$patientID, subset.values$patientID)
+      subset.points <- subset.points[order(sorting), ]
+      subset.points <- cbind(subset.points, subset.values$value)
+      jitter <- runif(nrow(subset.points), -0.5, 0.5)
+      subset.points <- cbind(subset.points, jitter)
+      names(subset.points) <- c('patientID', 'subset', 'value', 'jitter')
+      assignedPatientIDs <- c(assignedPatientIDs, subset.points$patientID)
+      bxp <- boxplot(subset.points$value, plot=FALSE)
+      data[[subset]] <- list()
+      data[[subset]]$lowerWhisker <- bxp$stats[1]
+      data[[subset]]$lowerHinge <- bxp$stats[2]
+      data[[subset]]$median <- bxp$stats[3]
+      data[[subset]]$upperHinge <- bxp$stats[4]
+      data[[subset]]$upperWhisker <- bxp$stats[5]
+      outlier <- subset.points$value > data[[subset]]$upperWhisker | subset.points$value < data[[subset]]$lowerWhisker
+      subset.points <- cbind(subset.points, outlier)
+      data[[subset]]$points <- subset.points
+    }
+  }
+  data$subsets <- nonEmptySubsets
+  write(data,file="boxplot.json")
+  toJSON(data, pretty=TRUE)
 }
