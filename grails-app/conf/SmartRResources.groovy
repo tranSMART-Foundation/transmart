4e93add68d8dbf202eff3858dc49d480055d72ca
Sascha Herzinger <sascha.herzinger@uni.lu>
2016-01-20 15:30:34 +0100

    smartR.es6 -> smartR.js

--- grails-app/conf/SmartRResources.groovy
+++ grails-app/conf/SmartRResources.groovy
@@ -5,7 +5,7 @@ modules = {
         resource url: [plugin: 'smart-r', dir: 'js/resource', file: 'jquery-ui-1.11.4.min.js']
         resource url: [plugin: 'smart-r', dir: 'js/resource', file: 'jquery.tablesorter.min.js']
         resource url: [plugin: 'smart-r', dir: 'js/resource', file: 'd3.min.js']
-        resource url: [plugin: 'smart-r', dir: 'js/smartR', file: 'smartR-compiled.js']
+        resource url: [plugin: 'smart-r', dir: 'js/smartR', file: 'smartR.js']
         resource url: [plugin: 'smart-r', dir: 'js/resource', file: 'polyfill.min.js']
     }
 
@@ -19,6 +19,6 @@ modules = {
         resource url: [plugin: 'smart-r', dir: 'js/smartR/Heatmap', file: 'heatmapValidator.js']
         resource url: [plugin: 'smart-r', dir: 'js/smartR/Heatmap', file: 'heatmapService.js']
         resource url: [plugin: 'smart-r', dir: 'js/smartR/Heatmap', file: 'heatmapView.js']
-        resource url: [plugin: 'smart-r', dir: 'js/smartR/Heatmap', file: 'd3Heatmap-compiled.js']
+        resource url: [plugin: 'smart-r', dir: 'js/smartR/Heatmap', file: 'd3Heatmap.js']
     }
 }
