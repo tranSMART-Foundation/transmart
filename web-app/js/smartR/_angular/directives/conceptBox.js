7e977f21de2318e07949186a9dbdea67febff52a
Sascha Herzinger <sascha.herzinger@uni.lu>
2016-02-03 15:49:43 +0100

    woho, drag & drop works automatically

--- web-app/js/smartR/_angular/directives/conceptBox.js
+++ web-app/js/smartR/_angular/directives/conceptBox.js
@@ -8,13 +8,21 @@ smartRApp.directive('conceptBox', function() {
         },
         template: '<div class="queryGroupIncludeSmall"></div>',
         controller: function($scope) {
-            $scope.conceptBoxes[$scope.conceptGroup] = [];
+            //$scope.conceptBoxes[$scope.conceptGroup] = [];
         },
-        link: function(scope, element, attrs) {
-            //scope.$watch(
-            //    function() { return element[0].childNodes.length; },
-            //    function() { console.log(element); scope.conceptBoxes[scope.conceptGroup] = getConcepts(el); }
-            //)
+        link: function(scope, elements, attrs) {
+
+            // activate drag & drop for template element
+            scope.$evalAsync(function() {
+                var div = Ext.get(elements[0]);
+                var dtgI = new Ext.dd.DropTarget(div, {ddGroup: 'makeQuery'});
+                dtgI.notifyDrop = dropOntoCategorySelection;
+            });
+
+            scope.$watch(
+                function() { return elements[0].childNodes.length; },
+                function() { console.log(elements[0]); scope.conceptBoxes[scope.conceptGroup] = getConcepts(elements[0]); }
+            );
         }
     };
-}); 
+});
