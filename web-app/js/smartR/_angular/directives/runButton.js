b1192fd63041fb92a32c6369d08788dc3bc1520a
Sascha Herzinger <sascha.herzinger@uni.lu>
2016-02-11 08:37:06 +0100

    more eye candy

--- web-app/js/smartR/_angular/directives/runButton.js
+++ web-app/js/smartR/_angular/directives/runButton.js
@@ -8,20 +8,30 @@ smartRApp.directive('runButton', ['rServeService', function(rServeService) {
             name: '@buttonName',
             arguments: '=parameterMap'
         },
-        template: '<input type="button" value="{{name}}"><span style="color:red"></span>',
+        template: '<input type="button" value="{{name}}"><span style="padding-left: 10px;"></span>',
         link: function(scope, element) {
             var template_btn = element.children()[0];
             var template_msg = element.children()[1];
 
             template_btn.onclick = function() {
-                template_msg.innerHTML = '';
+                template_btn.disabled = true;
+                template_msg.style.color = 'black';
+                template_msg.innerHTML = 'Creating plot, please wait <span class="blink_me">_</span>';
                 rServeService.startScriptExecution({
                     taskType: scope.script,
                     arguments: scope.arguments
                 }).then(
-                    function (response) { scope.storage = JSON.parse(response.result.artifacts.value); },
-                    function (response) { template_msg.innerHTML = '  Failure: ' + response.statusText; }
-                );
+                    function (response) {
+                        template_msg.innerHTML = '';
+                        scope.storage = JSON.parse(response.result.artifacts.value);
+                    },
+                    function (response) {
+                        template_msg.style.color = 'red';
+                        template_msg.innerHTML = '  Failure: ' + response.statusText;
+                    }
+                ).finally(function() {
+                    template_btn.disabled = false;
+                });
             };
         }
     };
