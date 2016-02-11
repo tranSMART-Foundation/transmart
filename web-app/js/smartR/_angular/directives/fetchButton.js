64aa8f97e31f3a9a395a59c08ca0757450a6687a
Sascha Herzinger <sascha.herzinger@uni.lu>
2016-02-11 08:31:43 +0100

    some eye candy

--- web-app/js/smartR/_angular/directives/fetchButton.js
+++ web-app/js/smartR/_angular/directives/fetchButton.js
@@ -5,18 +5,21 @@ smartRApp.directive('fetchButton', ['rServeService', 'smartRUtils', function(rSe
         scope: {
             conceptMap: '='
         },
-        template: '<input type="button" value="Fetch Data"><span></span>',
+        template: '<input type="button" value="Fetch Data"><span style="padding-left: 10px;"></span>',
         link: function(scope, element) {
             var template_btn = element.children()[0];
             var template_msg = element.children()[1];
 
             template_btn.onclick = function() {
-                template_msg.innerHTML = '';
+                template_btn.disabled = true;
+                template_msg.innerHTML = 'Fetching data, please wait <span class="blink_me">_</span>';
                 var conceptKeys = smartRUtils.conceptBoxMapToConceptKeys(scope.conceptMap);
                 rServeService.loadDataIntoSession(conceptKeys).then(
-                    function(msg) { template_msg.innerHTML = '  Success: ' + msg; },
-                    function(msg) { template_msg.innerHTML = '  Failure: ' + msg; }
-                );
+                    function(msg) { template_msg.innerHTML = 'Success: ' + msg; },
+                    function(msg) { template_msg.innerHTML = 'Failure: ' + msg; }
+                ).finally(function() {
+                    template_btn.disabled = false;
+                });
             };
         }
     };
