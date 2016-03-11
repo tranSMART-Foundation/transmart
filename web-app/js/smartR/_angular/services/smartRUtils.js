8857af65ab58ad77ab5c815d36d26f2717a9a887
Sascha Herzinger <sascha.herzinger@uni.lu>
2016-03-11 11:26:40 +0100

    minor improvements

--- web-app/js/smartR/_angular/services/smartRUtils.js
+++ web-app/js/smartR/_angular/services/smartRUtils.js
@@ -19,8 +19,8 @@ window.smartRApp.factory('smartRUtils', ['$q', function($q) {
         return String(str).replace(/[^a-z0-9]/g, function(s) {
             var c = s.charCodeAt(0);
             if (c == 32) return '-';
-            if (c >= 65 && c <= 90) return s.toLowerCase();
-            return ('000' + c.toString(16)).slice(-4);
+            if (c >= 65 && c <= 90) return '_' + s.toLowerCase();
+            return '__' + ('000' + c.toString(16)).slice(-4);
         });
     };
 
