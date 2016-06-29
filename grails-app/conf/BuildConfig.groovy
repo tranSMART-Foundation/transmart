bbb6e1ae923332048f373d959177d9b0971cc29a
Terry Weymouth <weymouth@umich.edu>
2016-06-29 13:47:48 -0400

    update version number to 16.2-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -55,7 +55,7 @@ grails.project.dependency.resolution = {
 
         /* serializable ImmutableMap only on guava 16 */
         compile group: 'com.google.guava', name: 'guava', version: '16.0-dev-20140115-68c8348'
-        compile 'org.transmartproject:transmart-core-api:1.2.2-SNAPSHOT'
+        compile 'org.transmartproject:transmart-core-api:16.2-SNAPSHOT'
         //test 'com.jayway.restassured:rest-assured:2.4.1'
 
         runtime 'org.javassist:javassist:3.16.1-GA'
@@ -83,10 +83,10 @@ grails.project.dependency.resolution = {
         //runtime ':hibernate:3.6.10.16'
 
         if (!dm) {
-            runtime ':transmart-core:1.2.2-SNAPSHOT'
+            runtime ':transmart-core:16.2-SNAPSHOT'
 
-            test ':transmart-core:1.2.2-SNAPSHOT'
-            test ':transmart-core-db-tests:1.2.2-SNAPSHOT'
+            test ':transmart-core:16.2-SNAPSHOT'
+            test ':transmart-core-db-tests:16.2-SNAPSHOT'
         } else {
             dm.internalDependencies delegate
         }
