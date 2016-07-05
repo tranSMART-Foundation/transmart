d8afab466dfab04dd6f6caa3bf0df5fd558cfdc8
Sascha Herzinger <sascha.herzinger@uni.lu>
2016-07-05 08:10:38 +0200

    :fish:

--- web-app/js/smartR/_angular/viz/d3Linegraph.js
+++ web-app/js/smartR/_angular/viz/d3Linegraph.js
@@ -29,6 +29,7 @@ window.smartRApp.directive('lineGraph', [
             var width = parseInt(scope.width) - margin.left - margin.right;
             var height = parseInt(scope.height) - margin.top - margin.bottom;
             var data_matrix = scope.data.data_matrix;
+
             var cf = crossfilter(data_matrix);
             var byPatientID = cf.dimension(function(d) { return d.patientID; });
             var byValue = cf.dimension(function(d) { return d.value; });
@@ -36,40 +37,40 @@ window.smartRApp.directive('lineGraph', [
             var byFullName = cf.dimension(function(d) { return d.fullName; });
             var byType = cf.dimension(function(d) { return d.type; });
             var bySubset = cf.dimension(function(d) { return d.subset; });
-            
-            var times = data_matrix.reduce(function(prev, curr) { 
-                var arr = prev;
-                var hits = arr.filter(function(d) { return d.time === curr.time; });
-                if (hits.length === 0) {
-                    arr.push({time: curr.time, name: curr.name});
-                }
-                return arr;
-            }, []);
-            times.sort(function(a, b) { return a.time - b.time; });
 
-
+            var groupByPatientID = byPatientID.group();
             var groupByFullName = byFullName.group();
+            var groupByTime = byTime.group();
+
+            var shownPatients = groupByPatientID.all().map(function(d) { return d.key; }).slice(0, 5); // TODO
+            (function resetPatientIDDimension() {
+                byPatientID.filterExact(function(d) { return shownPatients.indexOf(d.patientID) !== -1; });
+            })();
+
+            function getNonZeroGroupKeys(group) {
+                return group.all()
+                    .filter(function(d) { return d.value > 0; })
+                    .map(function(d) { return d.key; });
+            }
+
+            var x = d3.scale.linear();
+            // recomputes x scale for current filters
+            (function calculateXScale() {
+                var times = getNonZeroGroupKeys(groupByTime);
+                var xTicks = times.map(function(time, i) {
+                    if (i === 0) { return 0; }
+                    return times[times.length - 1] / time * width;
+                });
+                x.domain(times).range(xTicks);
+            })();
 
             byType.filterExact('categoric');
-            var catFullNames = groupByFullName.all()
-                .filter(function(d) { return d.value > 0; })
-                .map(function(d) { return d.key; });
+            var catFullNames = getNonZeroGroupKeys(groupByFullName);
 
             byType.filterExact('numeric');
-            var numFullNames = groupByFullName.all()
-                .filter(function(d) { return d.value > 0; })
-                .map(function(d) { return d.key; });
+            var numFullNames = getNonZeroGroupKeys(groupByFullName);
 
             byType.filterAll();
-            groupByFullName.dispose();
-
-            var xTicks = times.map(function(d, i) {
-                if (i === 0) { return 0; }
-                return times[times.length - 1] / d * width;
-            });
-            var x = d3.scale.linear()
-                .domain(times)
-                .range(xTicks);
 
             var svg = d3.select(root).append('svg')
                 .attr('width', width + margin.left + margin.right)
@@ -79,11 +80,37 @@ window.smartRApp.directive('lineGraph', [
 
             var xAxis = d3.svg.axis()
                 .scale(x)
-                .tickValues(times.map(function() { return d.time; }));
+                .tickValues(getNonZeroGroupKeys(groupByTime));
 
             svg.append('g')
                 .attr('class', 'sr-linegraph-x-axis')
                 .call(xAxis);
+
+            // returns size of highest stack of categories for a certain patient at all times
+            function getCategoricalDensity(patientID) {
+                var tempByPatientID = cf.dimension(function(d) { return d.patientID; });
+                var tempByTime = cf.dimension(function(d) { return d.time; });
+
+                tempByPatientID.filterExact(patientID);
+                var maxCount = 0;
+				getNonZeroGroupKeys(groupByTime).forEach(function(time) {
+					tempByTime.filterExact(time);
+                    var count = byValue.top(Infinity).length;
+                    maxCount = count > maxCount ? count : maxCount;
+                });
+
+                tempByPatientID.dispose();
+                tempByTime.dispose();
+                return maxCount;
+            }
+
+            byType.filterExact('categoric');
+            var categoricalDensities = getNonZeroGroupKeys(groupByPatientID).map(function(patientID) {
+                return getCategoricalDensity(patientID);
+            });
+            byType.filterAll();
+
+            console.log(categoricalDensities);
         }
 
     }
