81577e0c4de5cdddd848b0644aac0c613abcbea5
Sascha Herzinger <sascha.herzinger@uni.lu>
2016-07-07 09:33:10 +0200

    Boxes are dynamically selectable now.

--- web-app/js/smartR/_angular/viz/d3Linegraph.js
+++ web-app/js/smartR/_angular/viz/d3Linegraph.js
@@ -4,7 +4,8 @@
 
 window.smartRApp.directive('lineGraph', [
     'smartRUtils',
-    function(smartRUtils) {
+    '$rootScope',
+    function(smartRUtils, $rootScope) {
 
         return {
             restrict: 'E',
@@ -13,23 +14,28 @@ window.smartRApp.directive('lineGraph', [
                 width: '@',
                 height: '@'
             },
+            templateUrl: $rootScope.smartRPath + '/js/smartR/_angular/templates/linegraph.html',
             link: function (scope, element) {
+                var template_ctrl = element.children()[0],
+                    template_viz = element.children()[1];
+
                 scope.$watch('data', function() {
-                    $(element[0]).empty();
+                    $(template_viz).empty();
                     if (! $.isEmptyObject(scope.data)) {
                         smartRUtils.prepareWindowSize(scope.width, scope.height);
-                        createLinegraph(scope, element[0]);
+                        createLinegraph(scope, template_viz, template_ctrl);
                     }
                 });
             }
         };
 
-        function createLinegraph(scope, root) {
+        function createLinegraph(scope, vizDiv) {
             var MARGIN = {top: 50, right: 50, bottom: 50, left: 50};
             var LINEGRAPH_WIDTH = parseInt(scope.width) - MARGIN.left - MARGIN.right;
             var LINEGRAPH_HEIGHT = parseInt(scope.height) - MARGIN.top - MARGIN.bottom;
 
             var CAT_PLOTS_HEIGHT = LINEGRAPH_HEIGHT / 2; // FIXME: make dynamic
+            var NUM_PLOTS_HEIGHT = LINEGRAPH_HEIGHT / 2; // FIXME: make dynamic
             var CAT_PLOTS_OFFSET_BOTTOM = 20;
 
             var data_matrix = scope.data.data_matrix;
@@ -40,15 +46,28 @@ window.smartRApp.directive('lineGraph', [
             var byTime = cf.dimension(function(d) { return d.time; });
             var byFullName = cf.dimension(function(d) { return d.fullName; });
             var byType = cf.dimension(function(d) { return d.type; });
+            var bySubset = cf.dimension(function(d) { return d.subset; });
 
             var groupByPatientID = byPatientID.group();
             var groupByFullName = byFullName.group();
             var groupByTime = byTime.group();
+            var groupBySubset = bySubset.group();
 
-            var shownPatients = groupByPatientID.all().map(function(d) { return d.key; }).slice(0, 5); // TODO
-            (function resetPatientIDDimension() {
-                byPatientID.filterExact(function(d) { return shownPatients.indexOf(d.patientID) !== -1; });
-            })();
+            var patientRange = smartRUtils.getElementWithoutEventListeners('sr-linegraph-patient-range');
+            patientRange.min = 0;
+            patientRange.max = getNonZeroGroupKeys(groupByPatientID).length;
+            patientRange.value = 5;
+            patientRange.step = 1;
+            patientRange.addEventListener('change', function() {
+                numOfPatientsToShow(patientRange.value);
+                renderCategoricPlots();
+            });
+
+            function numOfPatientsToShow(num) {
+                var shownPatients = getNonZeroGroupKeys(groupByPatientID).slice(0, num); // show top 5 initially
+                byPatientID.filterFunction(function(patient) { return shownPatients.indexOf(patient) !== -1; });
+            }
+            numOfPatientsToShow(5);
 
             function getNonZeroGroupKeys(group) {
                 return group.all()
@@ -58,13 +77,14 @@ window.smartRApp.directive('lineGraph', [
 
             var x = d3.scale.linear();
             // recomputes x scale for current filters
-            (function calculateXScale() {
+            function calculateXScale() {
                 var times = getNonZeroGroupKeys(groupByTime);
                 var xTicks = times.map(function(time) {
                     return (time - times[0]) / (times[times.length - 1] - times[0]) * LINEGRAPH_WIDTH;
                 });
                 x.domain(times).range(xTicks);
-            })();
+            }
+            calculateXScale();
 
             byType.filterExact('categoric');
             var catFullNames = getNonZeroGroupKeys(groupByFullName);
@@ -73,7 +93,7 @@ window.smartRApp.directive('lineGraph', [
 
             byType.filterAll();
 
-            var svg = d3.select(root).append('svg')
+            var svg = d3.select(vizDiv).append('svg')
                 .attr('width', LINEGRAPH_WIDTH + MARGIN.left + MARGIN.right)
                 .attr('height', LINEGRAPH_HEIGHT + MARGIN.top + MARGIN.bottom)
                 .append('g')
@@ -88,27 +108,35 @@ window.smartRApp.directive('lineGraph', [
                 .attr('transform', 'translate(' + 0 + ',' + LINEGRAPH_HEIGHT + ')')
                 .call(xAxis);
 
-            // FIXME: I probably should use new dimensions rather than resetting possibly used ones
-            (function renderCategoricPlots() {
+            function renderCategoricPlots() {
                 byType.filterExact('categoric');
+                // temporary dimensions because we want to keep filters within this function scope
+                var tmpByPatientID = cf.dimension(function(d) { return d.patientID; });
+                var tmpByTime = cf.dimension(function(d) { return d.time; });
+
                 var catBoxInfo = getNonZeroGroupKeys(groupByPatientID).map(function(patientID) {
-                    byPatientID.filterExact(patientID);
+                    tmpByPatientID.filterExact(patientID);
                     var maxCount = 0;
                     getNonZeroGroupKeys(groupByTime).forEach(function(time) {
-                        byTime.filterExact(time);
+                        tmpByTime.filterExact(time);
                         var count = byValue.top(Infinity).length;
                         maxCount = count > maxCount ? count : maxCount;
+                        tmpByTime.filterAll();
                     });
+                    tmpByPatientID.filterAll();
                     return {patientID: patientID, maxDensity: maxCount};
                 });
-                byType.filterAll();
-                byPatientID.filterAll();
-                byTime.filterAll();
 
                 var totalDensity = catBoxInfo.reduce(function(prev, curr) { return curr.maxDensity + prev; }, 0);
                 catBoxInfo.forEach(function(d) {
                     d.height = Math.floor(d.maxDensity / totalDensity * CAT_PLOTS_HEIGHT);
+                    tmpByPatientID.filterExact(d.patientID);
+                    d.subset = getNonZeroGroupKeys(groupBySubset);
                 });
+
+                byType.filterAll();
+                tmpByPatientID.dispose();
+                tmpByTime.dispose();
 
                 var catBox = svg.selectAll('.sr-linegraph-cat-box')
                     .data(catBoxInfo);
@@ -119,7 +147,12 @@ window.smartRApp.directive('lineGraph', [
                         return 'sr-linegraph-cat-box' + ' ' + smartRUtils.makeSafeForCSS('patientid-' + d.patientID);
                     })
                     .attr('x', 0)
-                    .attr('width', LINEGRAPH_WIDTH);
+                    .attr('width', LINEGRAPH_WIDTH)
+                    .attr('fill', function(d) {
+                        if (d.subset.length === 2) { return 'rgba(255, 255, 0, 0.5)'; }
+                        if (d.subset[0] === 1) { return 'rgba(0, 255, 0, 0.5)'; }
+                        return 'rgba(255, 0, 0, 0.5)';
+                    });
 
                 catBox
                     .attr('y', function(d, i) {
@@ -130,8 +163,8 @@ window.smartRApp.directive('lineGraph', [
                         return LINEGRAPH_HEIGHT - CAT_PLOTS_OFFSET_BOTTOM - previousHeight + d.height;
                     })
                     .attr('height', function(d) { return d.height; });
-            })();
+            }
+            renderCategoricPlots();
         }
-
     }
 ]);
