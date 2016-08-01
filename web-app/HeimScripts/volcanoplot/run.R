5fadc58a13a147b2b503e4de362751b82b5cc3fc
Wim van der Linden <wim.van.der.linden@philips.com>
2016-08-01 17:42:22 +0200

    Remove rows for which the log fold change value is NA

--- web-app/HeimScripts/volcanoplot/run.R
+++ web-app/HeimScripts/volcanoplot/run.R
@@ -62,6 +62,7 @@ main <- function() {
 
   df          <- addStats(df, sorting, ranking, max_rows)
   df          <- mergeDuplicates(df)
+  df          <- df[!is.na(df["LOGFOLD"][,1]),] # remove rows with NA valued logFC
   df          <- df[1:min(max_rows, nrow(df)), ]  #  apply max_rows
 
   fields      <- buildFields(df)
