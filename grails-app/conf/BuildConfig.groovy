0cbb15844a58a381e3546c1fee2800ed475a543d
Peter Rice <p.rice@imperial.ac.uk>
2016-10-17 15:19:30 +0100

    repo URL

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,7 +15,7 @@ grails.project.dependency.resolution = {
         grailsCentral()
         mavenLocal()
         mavenCentral()
-        mavenRepo "https://repo.thehyve.nl/content/groups/public/"
+        mavenRepo "https://repo.transmartfoundation.org/content/groups/public/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
