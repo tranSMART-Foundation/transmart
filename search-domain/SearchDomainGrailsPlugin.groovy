7505b2aeb26183e01c6d436d816a7963d22ae2b4
Peter Rice <p.rice@imperial.ac.uk>
2016-12-12 15:22:48 +0000

    version release 16.2

--- search-domain/SearchDomainGrailsPlugin.groovy
+++ search-domain/SearchDomainGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class SearchDomainGrailsPlugin {
     // the plugin version
-    def version = "16.2-SNAPSHOT"
+    def version = "16.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
--- search-domain/grails-app/conf/BuildConfig.groovy
+++ search-domain/grails-app/conf/BuildConfig.groovy
@@ -27,8 +27,8 @@ grails.project.dependency.resolution = {
 		build(":release:3.0.1",
 			":rest-client-builder:1.0.3") {
 		export = false}
-        compile ':biomart-domain:16.2-SNAPSHOT'
+        compile ':biomart-domain:16.2'
         //// already included in biomart-domain
-        //compile ':transmart-java:16.2-SNAPSHOT'
+        //compile ':transmart-java:16.2'
     }
 }
