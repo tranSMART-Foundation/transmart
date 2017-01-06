d4afc69b24363096f581540f25905e72ef23fc9c
Sascha Herzinger <sascha.herzinger@uni.lu>
2017-01-06 15:19:17 +0100

    fixed regex that only matched integers for patient IDs

--- web-app/HeimScripts/heatmap/run.R
+++ web-app/HeimScripts/heatmap/run.R
@@ -99,7 +99,7 @@ main <- function(max_rows = 100, sorting = "nodes", ranking = "coef", selections
     rowNames        <- hd.df[, 1]
     
     ## colNames should reflect here only the sample names (e.g. "67_Breast_s1")
-    colNames = colnames(hd.df)[grep("^\\d+_.+_s\\d$", colnames(hd.df), perl = TRUE)]
+    colNames = colnames(hd.df)[grep("^.+_.+_s\\d$", colnames(hd.df), perl = TRUE)]
 
     significanceValues <- hd.df["SIGNIFICANCE"][,1]
     
