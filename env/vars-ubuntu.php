2ff03bb91796289ecb228ab0a8a2980a00314fc5
Peter Rice <p.rice@imperial.ac.uk>
2017-01-24 15:23:10 +0000

    vars-ubuntu update Kettle-ETL path, standard variable list for export

--- env/vars-ubuntu.php
+++ env/vars-ubuntu.php
@@ -6,10 +6,12 @@ PGPASSWORD=
 
 PGSQL_BIN="sudo -E -u postgres /usr/bin/"
 TABLESPACES=/var/lib/postgresql/tablespaces/
-KETTLE_JOBS_PSQL=<?= realpath(__DIR__), "/tranSMART-ETL/Postgres/GPL-1.0/Kettle/Kettle-ETL/", "\n" ?>
+KETTLE_JOBS_PSQL=<?= realpath(__DIR__), "/tranSMART-ETL/Kettle/postgres/Kettle-ETL/", "\n" ?>
 KITCHEN=<?= realpath(__DIR__), "/data-integration/kitchen.sh", "\n" ?>
 
 PATH=<?= realpath(__DIR__) ?>:$PATH
 
 export PGHOST PGPORT PGDATABASE PGUSER PGPASSWORD TABLESPACES PGSQL_BIN \
-	KETTLE_JOBS_PSQL KITCHEN PATH
+	R_JOBS_PSQL KETTLE_JOBS_PSQL KETTLE_JOBS_ORA KITCHEN TSUSER_HOME ORAHOST ORAPORT \
+	ORASID ORASVC ORAUSER ORAPASSWORD ORACLE_MANAGE_TABLESPACES \
+	ORACLE_TABLESPACES_DIR ORACLE PATH
