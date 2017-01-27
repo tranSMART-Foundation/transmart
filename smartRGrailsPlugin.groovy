65bf22b8d6ca9034276145b69661540b55369f15
Sascha Herzinger <sascha.herzinger@uni.lu>
2017-01-27 08:53:11 +0100

    Update smartRGrailsPlugin.groovy

--- smartRGrailsPlugin.groovy
+++ smartRGrailsPlugin.groovy
@@ -10,7 +10,7 @@ class smartRGrailsPlugin {
     public static final String TRANSMART_EXTENSIONS_REGISTRY_BEAN_NAME = 'transmartExtensionsRegistry'
 
     // the plugin version
-    def version = "1.2-BETA-SNAPSHOT"
+    def version = "1.2-STABLE-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
