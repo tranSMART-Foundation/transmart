0a3e4d9229a7a6693bb40ac735aa247db10d5f78
Peter Rice <p.rice@imperial.ac.uk>
2017-03-09 14:23:07 +0000

    add 'NOT NULL' to qt_patient_sample_collection

--- ddl/postgres/i2b2demodata/qt_patient_sample_collection.sql
+++ ddl/postgres/i2b2demodata/qt_patient_sample_collection.sql
@@ -2,7 +2,7 @@
 -- Name: qt_patient_set_collection; Type: TABLE; Schema: i2b2demodata; Owner: -
 --
 CREATE TABLE qt_patient_sample_collection (
-    sample_id bigint,
-    patient_id bigint,
-    result_instance_id bigint
+    sample_id bigint NOT NULL,
+    patient_id bigint NOT NULL,
+    result_instance_id bigint NOT NULL
 );
