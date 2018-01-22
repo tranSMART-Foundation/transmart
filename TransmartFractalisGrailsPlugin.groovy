025fbf82982b88984d3b0aa9c91410f4870d8c96
Sascha Herzinger <sascha.herzinger@uni.lu>
2018-01-22 10:12:34 -0500

    Injecting fractalis into tranSMART via plugin system

--- TransmartFractalisGrailsPlugin.groovy
+++ TransmartFractalisGrailsPlugin.groovy
@@ -1,14 +1,40 @@
+import org.codehaus.groovy.grails.plugins.GrailsPluginUtils
+
 class TransmartFractalisGrailsPlugin {
-	def version = '0.1-SNAPSHOT'
-	def grailsVersion = '2.3 > *'
-	def title = 'Transmart Fractalis Plugin'
-	def author = 'Sascha Herzinger'
-	def authorEmail = 'TODO'
-	def description = 'TODO'
-	def documentation = 'TODO'
-	def license = 'APACHE'
-	def organization = [name: 'TODO', url: 'TODO']
-	// def developers = [ [ name: 'Joe Bloggs', email: 'joe@bloggs.net' ]]
-	def issueManagement = [url: 'TODO' ]
-	def scm = [url: 'TODO']
+
+	public static final String TRANSMART_EXTENSIONS_REGISTRY_BEAN_NAME = 'transmartExtensionsRegistry'
+
+	def version = "0.1-SNAPSHOT"
+	def grailsVersion = "2.3 > *"
+    def title = 'Transmart Fractalis Plugin'
+	def author = "Burt Beckwith & Sascha Herzinger"
+	def authorEmail = "Burt_Beckwith@hms.harvard.edu & Sascha_Herzinger@hms.harvard.edu"
+	def description = 'A plugin to connect i2b2-tranSMART with https://git-r3lab.uni.lu/Fractalis'
+	def documentation = ''
+    def license = 'APACHE'
+    def organization = [name: '', url: '']
+    def issueManagement = [url: 'TODO' ]
+    def scm = [url: 'TODO']
+
+	def doWithApplicationContext = { ctx ->
+		File fractalisDir = GrailsPluginUtils.getPluginDirForName('transmart-fractalis')?.file
+		if (!fractalisDir) {
+			String pluginPath = ctx.pluginManager.allPlugins.find {
+				it.name == 'transmart-fractalis'
+			}.pluginPath
+
+			fractalisDir = ctx.getResource(pluginPath).file
+		} else {
+			fractalisDir = new File(fractalisDir, 'web-app')
+		}
+		if (!fractalisDir) {
+			throw new RuntimeException('Could not determine directory for transmart-fractalis plugin')
+		}
+
+		if (ctx.containsBean(TRANSMART_EXTENSIONS_REGISTRY_BEAN_NAME)) {
+			ctx.getBean(TRANSMART_EXTENSIONS_REGISTRY_BEAN_NAME).registerAnalysisTabExtension(
+                    'transmart-fractalis', '/Fractalis/loadScripts', 'addFractalisPanel')
+		}
+
+	}
 }
--- grails-app/controllers/org/transmart/plugin/fractalis/FractalisController.groovy
+++ grails-app/controllers/org/transmart/plugin/fractalis/FractalisController.groovy
@@ -1,5 +1,46 @@
 package org.transmart.plugin.fractalis
 
+import org.json.JSONArray
+import org.json.JSONObject
+
 class FractalisController {
+
 	def index() {}
+
+	/**
+	 *   Called to get the path to fractalis.js such that the plugin can be loaded in the datasetExplorer
+	 */
+	def loadScripts = {
+
+		// list of required javascript files
+		def scripts = [servletContext.contextPath + pluginContextPath + '/js/fractalis.js']
+
+		// list of required css files
+		def styles = []
+
+		JSONObject result = new JSONObject()
+		JSONArray rows = new JSONArray()
+
+		// for all js files
+		for (file in scripts) {
+			def m = [:]
+			m["path"] = file.toString()
+			m["type"] = "script"
+			rows.put(m)
+		}
+
+		// for all css files
+		for (file in styles) {
+			def n = [:]
+			n["path"] = file.toString()
+			n["type"] = "css"
+			rows.put(n)
+		}
+
+		result.put("success", true)
+		result.put("totalCount", scripts.size())
+		result.put("files", rows)
+
+		render result.toString()
+	}
 }
