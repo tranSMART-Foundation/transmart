1753dea33040e9be1cdee0f2a99946e5c39394cf
Burt Beckwith <burt@burtbeckwith.com>
2018-04-02 11:12:24 -0400

    added a jwtToken() method to generate a JWT token when not using Auth0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,12 +10,16 @@ grails.project.dependency.resolution = {
 		mavenLocal() // Note: use 'grails maven-install' to install required plugins locally
 		grailsCentral()
 		mavenCentral()
-		mavenRepo 'http://ec2-35-170-59-132.compute-1.amazonaws.com:8080/artifactory/plugins-snapshots'
+		mavenRepo 'http://ec2-35-170-59-132.compute-1.amazonaws.com:8080/artifactory/libs-snapshots'
+		mavenRepo 'http://ec2-35-170-59-132.compute-1.amazonaws.com:8080/artifactory/libs-releases'
 		mavenRepo 'http://ec2-35-170-59-132.compute-1.amazonaws.com:8080/artifactory/plugins-releases'
+		mavenRepo 'http://ec2-35-170-59-132.compute-1.amazonaws.com:8080/artifactory/plugins-snapshots'
 		mavenRepo 'https://repo.transmartfoundation.org/content/repositories/public/'
 	}
 
 	dependencies {
+		compile 'com.auth0:java-jwt:3.2.0'
+		compile 'com.fasterxml.jackson.core:jackson-core:2.8.5'
 		test 'org.grails:grails-datastore-test-support:1.0.2-grails-2.4'
 	}
 
--- grails-app/services/org/transmart/plugin/shared/SecurityService.groovy
+++ grails-app/services/org/transmart/plugin/shared/SecurityService.groovy
@@ -1,8 +1,12 @@
 package org.transmart.plugin.shared
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import grails.plugin.springsecurity.SpringSecurityService
 import groovy.transform.CompileStatic
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.userdetails.UserDetails
 
@@ -10,11 +14,19 @@ import org.springframework.security.core.userdetails.UserDetails
  * @author <a href='mailto:burt_beckwith@hms.harvard.edu'>Burt Beckwith</a>
  */
 @CompileStatic
-class SecurityService {
+class SecurityService implements InitializingBean {
 
 	static transactional = false
 
 	@Autowired private SpringSecurityService springSecurityService
+
+	@Value('${grails.plugin.springsecurity.auth0.clientId:}')
+	private String clientId
+
+	@Value('${grails.plugin.springsecurity.auth0.clientSecret:}')
+	private String clientSecret
+
+	private Algorithm algorithm
 
 	/**
 	 * @return the current auth if authenticated
@@ -57,5 +69,22 @@ class SecurityService {
 	 */
 	void authenticateAs(String username) {
 		springSecurityService.reauthenticate username
+	}
+
+	String jwtToken() {
+		String email = currentUsername()
+		Date now = new Date()
+		JWT.create()
+				.withAudience(clientId)      // 'aud'
+				.withExpiresAt(now + 1)      // 'exp'
+				.withIssuedAt(now)           // 'iat'
+				.withIssuer(getClass().name) // 'iss'
+				.withSubject(email)          // 'sub'
+				.withClaim('email', email)
+				.sign(algorithm)
+	}
+
+	void afterPropertiesSet() {
+		algorithm = Algorithm.HMAC256(clientSecret)
 	}
 }
