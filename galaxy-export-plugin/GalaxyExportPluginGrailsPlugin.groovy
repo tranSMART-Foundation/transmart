74c4debbbc19be0a3ae6febf8a0dc12d65c69b38
Peter Rice <peter.rice@i2b2transmart.org>
2018-11-12 18:17:10 +0000

    clean up simple double quotes in galaxy-export

--- galaxy-export-plugin/GalaxyExportPluginGrailsPlugin.groovy
+++ galaxy-export-plugin/GalaxyExportPluginGrailsPlugin.groovy
@@ -3,18 +3,18 @@ class GalaxyExportPluginGrailsPlugin {
     public static final String TRANSMART_EXTENSIONS_REGISTRY_BEAN_NAME = 'transmartExtensionsRegistry'
 
     // the plugin version
-    def version = "16.4-SNAPSHOT"
+    def version = '16.4-SNAPSHOT'
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.3 > *"
+    def grailsVersion = '2.3 > *'
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+            'grails-app/views/error.gsp'
     ]
 
     // TODO Fill in these fields
-    def title = "Galaxy Export Plugin" // Headline display name of the plugin
-    def author = "Transmart Foundation"
-    def authorEmail = "support@transmartfoundation.org"
+    def title = 'Galaxy Export Plugin' // Headline display name of the plugin
+    def author = 'Transmart Foundation'
+    def authorEmail = 'support@transmartfoundation.org'
     def description = '''\
 Brief summary/description of the plugin.
 '''
@@ -25,19 +25,19 @@ Brief summary/description of the plugin.
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-    def license = "GPL3"
+    def license = 'GPL3'
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+//    def organization = [ name: 'My Company', url: 'http://www.my-company.com/' ]
 
     // Any additional developers beyond the author specified above.
-    def developers = [ [ name: "Axel Oehmichen", email: "ao1011@imperial.ac.uk" ]]
+    def developers = [ [ name: 'Axel Oehmichen', email: 'ao1011@imperial.ac.uk' ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+//    def issueManagement = [ system: 'JIRA', url: 'http://jira.grails.org/browse/GPMYPLUGIN' ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+//    def scm = [ url: 'http://svn.codehaus.org/grails-plugins/' ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
