635bd43cb26e4be23f37cae436f143b0e37dde65
Peter Rice <peter.rice@i2b2transmart.org>
2019-02-18 16:46:55 +0000

    updates to R install for issues since 16.3

--- transmart-data/R/rforge_pkg.R
+++ transmart-data/R/rforge_pkg.R
@@ -1,13 +1,17 @@
-required.packages <- c("reshape", "reshape2", "ggplot2", "data.table", "Cairo",
-		"snowfall", "gplots", "foreach", "doParallel", "visreg",
-		"pROC", "jsonlite", "RUnit", "shiny");
+required.packages <- c("Rserve");
 missing.packages <- function(required) {
 	return(required[
 		!(required %in% installed.packages()[,"Package"])]);
 }
 new.packages <- missing.packages(required.packages)
+if (!length(new.packages))
+	q();
 if (length(new.packages)) {
 	install.packages(new.packages, repos=Sys.getenv("CRAN_MIRROR"));
+}
+
+if (length(intersect(new.packages, c("Rserve")))) {
+	install.packages'"Rserve',,'http://www.rforge.net/')
 }
 
 if (length(missing.packages(required.packages))) {
