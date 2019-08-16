d5a288c170f17254abc99cedf1b143e937a3aa63
Peter Rice <peter.rice@i2b2transmart.org>
2019-08-16 12:17:22 +0100

    Rmodules highdimensional panel and scripts new locations

--- Rmodules/grails-app/controllers/com/recomdata/transmart/data/association/DataAssociationController.groovy
+++ Rmodules/grails-app/controllers/com/recomdata/transmart/data/association/DataAssociationController.groovy
@@ -14,8 +14,8 @@ class DataAssociationController {
 	'RmodulesView.js',
 	'dataAssociation.js',
 	'PDFGenerator.js',
-	'ext/tsmart-overrides.js',
-	'ext/tsmart-generic.js',
+	'rmodules-tsmart-overrides.js',
+	'rmodules-tsmart-generic.js',
 	'plugin/IC50.js'].asImmutable()
     
     //TODO: requires images: servletContext.contextPath+pluginContextPath+'/css/jquery-ui-1.10.3.custom.css']
@@ -27,39 +27,29 @@ class DataAssociationController {
      * Load the initial DataAssociation page.
      */
     def defaultPage() {
-	logger.info 'defaultPage dataAssociation contextPath {}', pluginContextPath
         render template: 'dataAssociation', contextPath: pluginContextPath
     }
 
     def variableSelection(String analysis) {
-	logger.info 'variableSelection view analysis {} ../plugin/ + {}', analysis, pluginService.findPluginModuleByModuleName(analysis).formPage
-        render view: servletContext.contextPath + pluginService.findPluginModuleByModuleName(analysis).formPage
+        render view: '../plugin/' + pluginService.findPluginModuleByModuleName(analysis).formPage
     }
 
     /**
      * Load required scripts for running analysis
      */
     def loadScripts() {
-
 	List<Map> rows = []
-	logger.info 'also reporting servletContext.contextPath{}', servletContext.contextPath
-	logger.info 'also reporting defaultPage dataAssociation contextPath {}', pluginContextPath
-	logger.info 'also reporting variableSelection view analysis {} ../plugin/ + {}', 'PCA', pluginService.findPluginModuleByModuleName('PCA')
+
 	for (String script in scripts) {
 	    Resource assetRes = assetResourceLocator.findAssetForURI(script)
-//	    Resource resourceRes = assetResourceLocator.findResourceForURI(script)
-	    logger.info 'loading DataAssociation script {} asset {}', script, assetRes.getPath()
-//	    rows << [path: servletContext.contextPath + pluginContextPath + '/assets/' + script, type: 'script']
 	    rows << [path: servletContext.contextPath + assetRes.getPath(), type: 'script']
 	}
 
 	for (String style in styles) {	
 	    Resource assetRes = assetResourceLocator.findAssetForURI(style)
-	    logger.info 'loading DataAssociation style {} asset {}', style, assetRes.getPath()
-//	    rows << [path: servletContext.contextPath + pluginContextPath + '/assets/' + style, type: 'css']
 	    rows << [path: servletContext.contextPath + assetRes.getPath(), type: 'css']
 	}
 
-	render([success: true, totalCount: scripts.size(), files: rows] as JSON)
+	render([success: true, totalCount: rows.size(), files: rows] as JSON)
     }
 }
