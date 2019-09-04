85616ecfe084c24ee353c10b21a2ffd562a8bb3e
Peter Rice <peter.rice@i2b2transmart.org>
2019-09-04 10:48:14 +0100

    We do not need i2b2transmart-quick-start

--- i2b2transmart-quick-start/config/fractalis/config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = 'OVERWRITE ME IN PRODUCTION!!!'
-REDIS_HOST = 'redis'
-BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
-CELERY_RESULT_BACKEND = 'redis://redis:6379'
-ETL_VERIFY_SSL_CERT = False
