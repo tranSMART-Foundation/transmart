cfb95d722c1f00e44acf5401da060b0d506fd8af
Peter Rice <peter.rice@i2b2transmart.org>
2019-09-11 15:57:03 +0100

    Fix rnaseq annotation - pass GPL_ID in kettle and save in table

--- transmart-data/ddl/postgres/tm_cz/functions/i2b2_rna_seq_annotation.sql
+++ transmart-data/ddl/postgres/tm_cz/functions/i2b2_rna_seq_annotation.sql
@@ -1,101 +1,7 @@
 --
--- Name: i2b2_rna_seq_annotation(); Type: FUNCTION; Schema: tm_cz; Owner: -
---
-CREATE FUNCTION i2b2_rna_seq_annotation() RETURNS bigint
-    LANGUAGE plpgsql
-AS $$
-    declare
-
-    gpl_rtn bigint;
-    newJobFlag numeric(1);
-    databaseName character varying(100);
-    procedureName character varying(100);
-    jobID bigint;
-    errorNumber		character varying;
-    errorMessage	character varying;
-    rowCt			numeric(18,0);
-    stepCt bigint;
-
-begin
-
-    stepCt := 0;
-
-    --Set Audit Parameters
-    newJobFlag := 0; -- False (Default)
-    jobID := -1;
-    
-    databaseName := 'tm_cz';
-    procedureName := 'i2b2_rna_seq_annotation';
-
-    --Audit JOB Initialization
-    --If Job ID does not exist, then this is a single procedure run and we need to create it
-    if(coalesce(jobID::text, '') = '' or jobID < 1) then
-	newJobFlag := 1; -- True
-	select cz_start_audit (procedureName, databaseName, jobID) into jobId;
-    end if;
-
-    stepCt := stepCt + 1;
-    perform cz_write_audit(jobId,databaseName,procedureName,'Starting i2b2_rna_seq_annotation',0,stepCt,'Done');
-    
-    -- insert into deapp.de_rnaseq_annotation
-    
-    select count(platform) into gpl_rtn
-      from deapp.de_gpl_info
-     where marker_type='RNASEQ'
-       and (platform is NOT NULL
-	    and platform::text <> '');
-    if gpl_rtn=0 then
-	perform cz_write_audit(jobId,databasename,procedurename,'Platform data missing from DEAPP.DE_GPL_INFO',1,stepCt,'ERROR');
-	perform tm_cz.cz_error_handler (jobID, procedureName, errorNumber, errorMessage);
-	perform cz_end_audit (jobId,'FAIL');
-	return 161;
-    end if;
-    
-    begin
-	insert into deapp.de_rnaseq_annotation 
-		    (gpl_id
-		    ,transcript_id 
-		    ,gene_symbol
-		    ,gene_id
-		    ,organism
-		    ,probeset_id)
-	select g.platform
-               ,a.transcript_id
-               ,a.gene_symbol
-               ,b.bio_marker_id
-               ,a.organism
-               ,pd.probeset_id
-          from tm_lz.lt_rnaseq_annotation a
-               ,(select platform from deapp.de_gpl_info where marker_type='RNASEQ') as g
-               ,biomart.bio_marker b
-               ,probeset_deapp pd
-         where b.bio_marker_name=a.gene_symbol
-           and a.transcript_id =pd.probeset;
-	get diagnostics rowCt := ROW_COUNT;	
-    exception
-	when others then
-	    errorNumber := SQLSTATE;
-	    errorMessage := SQLERRM;
-	--Handle errors.
-	    perform tm_cz.cz_error_handler (jobID, procedureName, errorNumber, errorMessage);
-	--End Proc
-	    perform tm_cz.cz_end_audit (jobID, 'FAIL');
-	    return -16;
-    end;
-    
-    stepCt := stepCt + 1;
-    performcz_write_audit(jobId,databaseName,procedureName,'Insert new probesets into de_rnaseq_annotation',rowCt,stepCt,'Done');
-    
-    return 0 ;
-
-end;
-
-$$;
-
---
 -- Name: i2b2_rna_seq_annotation(bigint); Type: FUNCTION; Schema: tm_cz; Owner: -
 --
-CREATE FUNCTION i2b2_rna_seq_annotation(currentjobid bigint DEFAULT NULL::bigint) RETURNS numeric
+CREATE FUNCTION i2b2_rna_seq_annotation( platformId character varying, currentjobid bigint DEFAULT NULL::bigint) RETURNS numeric
     LANGUAGE plpgsql
 AS $$
     declare
@@ -136,8 +42,7 @@ begin
     select count(platform) into gpl_rtn
       from deapp.de_gpl_info
      where marker_type='RNASEQ'
-       and (platform IS NOT NULL
-	    and platform::text <> '');
+       and platform = platformId;
     if gpl_rtn=0 then
 	perform cz_write_audit(jobId,databasename,procedurename,'Platform data missing from DEAPP.DE_GPL_INFO',1,stepCt,'ERROR');
 	perform tm_cz.cz_error_handler (jobID, procedureName, errorNumber, errorMessage);
@@ -153,7 +58,7 @@ begin
 		    ,gene_id
 		    ,organism)
 	select distinct (a.transcript_id)
-			,null
+			,platformId
 			,a.gene_symbol
 			,null
 			,a.organism
