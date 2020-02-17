8263b08190d6879b9296ad06279a3a4b98e9674c
Peter Rice <peter.rice@i2b2transmart.org>
2020-02-17 09:03:32 +0000

    rnaseq gene_id numeric

--- transmart-data/ddl/postgres/deapp/de_rnaseq_annotation.sql
+++ transmart-data/ddl/postgres/deapp/de_rnaseq_annotation.sql
@@ -5,7 +5,7 @@ CREATE TABLE de_rnaseq_annotation (
     gpl_id character varying(50),
     transcript_id character varying(50),
     gene_symbol character varying(50),
-    gene_id character varying(50),
+    gene_id int,
     organism character varying(30),
     probeset_id int
 );
