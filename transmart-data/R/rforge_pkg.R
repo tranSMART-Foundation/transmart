c7469f0c315d0ff8aae7ec16a616e2a25294530a
Peter Rice <peter.rice@i2b2transmart.org>
2020-04-21 12:43:10 +0100

    update R install for release 19.0

--- transmart-data/R/rforge_pkg.R
+++ transmart-data/R/rforge_pkg.R
@@ -10,10 +10,6 @@ if (length(intersect(new.packages, c("Rserve")))) {
 	install.packages('Rserve',,'http://www.rforge.net/')
 }
 
-if (length(new.packages)) {
-	install.packages(new.packages, repos=Sys.getenv("CRAN_MIRROR"));
-}
-
 if (length(missing.packages(required.packages))) {
 	warning('Some packages not installed');
 	quit(1);
--- transmart-data/R/rserve.php
+++ transmart-data/R/rserve.php
@@ -1,87 +1,100 @@
 <?php
-$r = "$_ENV[R_PREFIX]/bin/R";
+$rdir = "$_ENV[R_PREFIX]/lib/R";
 $c = $_ENV['RSERVE_CONF'];
+$l = $_ENV['RSERVE_LOG'];
 ?>
 #!/bin/bash
 
 ### BEGIN INIT INFO
 # Provides:             rserve
-# Required-Start:       $local_fs $remote_fs $network
-# Required-Stop:        $local_fs $remote_fs $network
+# Required-Start:       $local_fs $remote_fs $network $syslog
+# Required-Stop:        $local_fs $remote_fs $network $syslog
 # Default-Start:        2 3 4 5
 # Default-Stop:         0 1 6
 # Short-Description:    rserve
+# Description:    	Rserve server for tranSMART
 ### END INIT INFO
 
-# for when user is a variable
+# Variables to be defined in /etc/default/rserve
+# RSERVE_USER (required) username to execute rserve process
+# RSERVE_LOG
+# RSERVE_CONF
+
 if [[ -f /etc/default/rserve ]]; then
         . /etc/default/rserve
 fi
-if [[ -z $USER ]]; then
-        echo '$USER not defined' >&2
-        exit 1
+if [[ -z $RSERVE_USER ]]; then
+    echo '$RSERVE_USER not defined' >&2
+    exit 1
+fi
+if [[ -z $RSERVE_LOG ]]; then
+    RSERVE_LOG="<?= $l ?>"
+fi
+if [[ -z $RSERVE_CONF ]]; then
+    RSERVE_CONF="<?= $c ?>"
 fi
 
-function do_start {
-        # Rserve does not daemonize properly. We need to reopen stdout
-        # as /dev/null, otherwise we get all kinds of output
-        # The standard way to do it is to connect /dev/null to file
-        # descriptors 0, 1 and 2 only in the child (daemonized) process
-        # but obviously we are unable to do that
-        exec 7>&1
-        exec 1>/dev/null
-        sudo --user="$USER" "<?= $r ?>" CMD Rserve --quiet --vanilla >&7
-        EXIT_VAL=$?
-        exec 1>&7 7>&-
-        if [ $EXIT_VAL -eq 0 ]; then
-                echo "Rserve started"
-        else
-                echo "Failed starting Rserve"
-        fi
+NAME="rserve"
+DESC="R execution server for tranSMART"
+DAEMON="<?= $rdir ?>/bin/Rserve"
+
+export R_HOME="<?= $rdir ?>"
+export LD_LIBRARY_PATH="<?= $rdir ?>/lib"
+
+. /lib/lsb/init-functions
+
+PID=/var/run/rserve.pid
+
+running() {
+  kill -0 `cat ${PID}` > /dev/null 2>&1
 }
 
-function do_stop {
-        if pgrep -u "$USER" -f Rserve  > /dev/null
-        then
-                kill `pgrep -u "$USER" -f Rserve`
-                if [ $? -eq 0 ]; then
-                        echo "Rserve killed"
-                else
-                        echo "Failed killing Rserve"
-                fi
-        else
-                echo "nothing to stop; Rserve is not running"
-                exit 0
-        fi
+start_rserve() {
+  touch ${PID} && chown ${RSERVE_USER}:${RSERVE_USER} ${PID}
+  sudo --user ${RSERVE_USER} ${RDAEMON} CMD Rserve --quiet --RS-pidfile ${PID} --vanilla >>$RSERVE_LOG 2>&1
+  EXIT_VAL=$?
+  if [ $EXIT_VAL -eq 0 ]; then
+    echo "Rserve started"
+  else
+    echo "Failed starting Rserve"
+  fi
+}
+
+stop_rserve() {
+  start-stop-daemon --stop --quiet --pidfile ${PID} \
+        --exec ${DAEMON} || true
+}
+
+status_rserve() {
+  status_of_proc -p ${PID} $NAME $NAME && exit 0 || exit $?
 }
 
 case "$1" in
-        start)
-                do_start
-        ;;
-
-        stop)
-                do_stop
-        ;;
-
-        restart|reload|force-reload)
-                do_stop
-                do_start
-        ;;
-
-        status)
-                if pgrep -u "$USER" -f Rserve > /dev/null
-                then
-                        echo "Rserve service running."
-                        exit 0
-                else
-                        echo "Rserve is not running"
-                        exit 1
-                fi
-        ;;
-
-        *)
-                echo "Usage: $0 {start|stop|status|restart|force-reload|reload}" >&2
-                exit 1
-        ;;
+  start)
+    log_daemon_msg "Starting $DESC" "$NAME"
+    start_rserve
+    log_end_msg $?
+    ;;
+  stop)
+    log_daemon_msg "Stopping $DESC" "$NAME"
+    stop_rserve
+    log_end_msg $?
+   ;;
+  restart|force-reload)
+    log_daemon_msg "Restarting $DESC" "$NAME"
+    stop_rserve
+    sleep 1
+    start_rserve
+    log_end_msg $?
+    ;;
+  status)
+    status_rserve
+    ;;
+  *)
+    echo "Usage: $NAME {start|stop|restart|status}" >&2
+    exit 1
+    ;;
 esac
+
+
+exit 0
