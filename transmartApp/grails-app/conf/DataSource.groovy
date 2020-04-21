17006dadf3beccf85c395c7e6ac1f20cb502584d
Peter Rice <peter.rice@i2b2transmart.org>
2020-04-21 13:15:12 +0100

    Cleanup whitespace in DataSource.groovy

--- transmartApp/grails-app/conf/DataSource.groovy
+++ transmartApp/grails-app/conf/DataSource.groovy
@@ -4,67 +4,67 @@
 dataSource_oauth2 {
     dbCreate = 'update'
     driverClassName = 'org.h2.Driver'
-    formatSql = true
     logSql = true
+    formatSql = true
     password = ''
     url = "jdbc:h2:~/.grails/oauth2db;MVCC=TRUE;DB_CLOSE_ON_EXIT=FALSE;AUTO_SERVER=TRUE"
     username = 'sa'
 }
 
 hibernate {
-	cache {
-		use_query_cache = true
-		use_second_level_cache = true
+    cache {
+	use_query_cache = true
+	use_second_level_cache = true
 
-		// make sure hibernate.cache.provider_class is not being set
-		// see http://stackoverflow.com/a/3690212/127724 and the docs for the cache-ehcache plugin
-		region.factory_class = 'grails.plugin.cache.ehcache.hibernate.BeanEhcacheRegionFactory'
-//		region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
-	}
-//	flush.mode = 'manual'
-	format_sql = true
-	singleSession = true
-	use_sql_comments = true
+	// make sure hibernate.cache.provider_class is not being set
+	// see http://stackoverflow.com/a/3690212/127724 and the docs for the cache-ehcache plugin
+	region.factory_class = 'grails.plugin.cache.ehcache.hibernate.BeanEhcacheRegionFactory'
+//	region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
+    }
+//  flush.mode = 'manual'
+    singleSession = true
+    format_sql = true
+    use_sql_comments = true
 }
 
 hibernate_oauth2 {
-	cache {
-		use_query_cache = false
-		use_second_level_cache = false
-	}
-//	flush.mode = 'manual'
-	format_sql = true
-	singleSession = true
-	use_sql_comments = true
+    cache {
+	use_query_cache = false
+	use_second_level_cache = false
+    }
+//  flush.mode = 'manual'
+    singleSession = true
+    format_sql = true
+    use_sql_comments = true
 }
 
 environments {
-	test {
-		dataSource {
-			dbCreate = 'update'
-			driverClassName = 'org.h2.Driver'
-			formatSql = true
-			logSql = true
-			password = ''
-			url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;INIT=RUNSCRIPT FROM './h2_init.sql'"
-			username = 'sa'
-		}
-
-		dataSource_oauth2 {
-			dbCreate = 'update'
-			driverClassName = 'org.h2.Driver'
-			formatSql = true
-			logSql = true
-			password = ''
-			url = "jdbc:h2:mem:oauth2;MVCC=TRUE"
-			username = 'sa'
-		}
-
-		hibernate {
-			cache {
-				use_query_cache = false
-				use_second_level_cache = true
-			}
-		}
+    test {
+	dataSource {
+	    dbCreate = 'update'
+	    driverClassName = 'org.h2.Driver'
+	    formatSql = true
+	    logSql = true
+	    password = ''
+	    url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;INIT=RUNSCRIPT FROM './h2_init.sql'"
+	    username = 'sa'
 	}
+
+	dataSource_oauth2 {
+	    dbCreate = 'update'
+	    driverClassName = 'org.h2.Driver'
+	    formatSql = true
+	    logSql = true
+	    password = ''
+	    url = "jdbc:h2:mem:oauth2;MVCC=TRUE"
+	    username = 'sa'
+	}
+
+	hibernate {
+	    cache {
+		use_query_cache = false
+		use_second_level_cache = true
+	    }
+	}
+    }
 }
