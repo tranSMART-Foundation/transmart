a961a6a837027075442b40cd6acf2948dcf014f7
Peter Rice <peter.rice@i2b2transmart.org>
2020-05-06 16:53:08 +0100

    Add logger to PluginDetectorController

--- transmartApp/grails-app/controllers/PluginDetectorController.groovy
+++ transmartApp/grails-app/controllers/PluginDetectorController.groovy
@@ -1,11 +1,14 @@
+import groovy.util.logging.Slf4j
 import org.codehaus.groovy.grails.plugins.GrailsPluginManager
 import org.springframework.beans.factory.annotation.Autowired
 
+@Slf4j('logger')
 class PluginDetectorController {
 
     @Autowired private GrailsPluginManager pluginManager
 
     def checkPlugin(String pluginName) {
+	logger.info 'checkPlugin {}: {}', pluginName, pluginManager.hasGrailsPlugin(pluginName)
 	render pluginManager.hasGrailsPlugin(pluginName)
     }
 }
