69db4878580d33f3d42eba94608326f823f7bc44
Peter Rice <peter.rice@i2b2transmart.org>
2020-05-15 13:39:48 +0100

    updates to solr.service definition

--- transmart-data/solr/solr.service.php
+++ transmart-data/solr/solr.service.php
@@ -4,21 +4,19 @@ $l = $_ENV['SOLR_LOG'];
 $d = $_ENV['SOLR_PREFIX'];
 ?>
 [Unit]
-Description=Solr server for tranSMART
+Description=Apache Solr server for tranSMART
 Documentation=https://lucene.apache.org/solr/
 
 [Service]
 ExecStart=/usr/bin/java -Xmx1024m -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -Dsolr.solr.home=solr -jar start.jar
 User=<?= $u, "\n" ?>
-TimeoutSec=15s
-Restart=always
-Nice=19
+Restart=on-failure
 WorkingDirectory=<?= $d, "\n" ?>
 StandardOutput=<?= $l, "\n" ?>
 # test this for production:
 #ReadOnlyDirectories=/
 #ReadWriteDirectories=/var/cache/jobs
-#PrivateTmp=true
+PrivateTmp=true
 
 [Install]
 WantedBy=multi-user.target
