922b40d4ed3e29c029a8e5edb4dbd6f7dd87acef
Peter Rice <peter.rice@i2b2transmart.org>
2021-06-21 10:16:24 +0100

    Update i2b2_trial_nodes view to report i2b2 nodes as a trial

--- transmart-data/ddl/postgres/i2b2metadata/views/i2b2_trial_nodes.sql
+++ transmart-data/ddl/postgres/i2b2metadata/views/i2b2_trial_nodes.sql
@@ -2,10 +2,17 @@
 -- Name: i2b2_trial_nodes; Type: VIEW; Schema: i2b2metadata; Owner: -
 --
 CREATE VIEW i2b2metadata.i2b2_trial_nodes AS
-    SELECT DISTINCT ON (i2b2.c_comment) i2b2.c_fullname
-		    , "substring"(i2b2.c_comment, 7) AS trial
-      FROM i2b2metadata.i2b2
-     WHERE (i2b2.c_comment IS NOT NULL)
-     ORDER BY i2b2.c_comment
-	      , char_length((i2b2.c_fullname)::text);
-
+    SELECT DISTINCT ON (trial) c_fullname, trial
+        FROM (
+	    SELECT sourcesystem_cd
+                   , CASE WHEN i2b2.c_comment like 'trial:%'
+                          THEN "substring"(i2b2.c_comment, 7)
+       		          ELSE 'I2B2' END
+	              AS trial
+                   , char_length(i2b2.c_fullname) AS length
+    		   , CASE WHEN i2b2.c_comment like 'trial:%'
+                          THEN i2b2.c_fullname
+       		          ELSE '\i2b2\' END
+	              AS c_fullname
+    	    FROM i2b2metadata.i2b2) SUB
+    ORDER BY trial, length ASC;
