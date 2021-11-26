6f9345f925fd1d39d0ebf35bd7d5ff5e02891454
Peter Rice <peter.rice@i2b2transmart.org>
2021-11-26 19:04:45 +0000

    Update search_keywords sequence value for updated genes

--- transmart-data/ddl/postgres/searchapp/search_auth_user_sec_access.sql
+++ transmart-data/ddl/postgres/searchapp/search_auth_user_sec_access.sql
@@ -55,7 +55,7 @@ ALTER TABLE ONLY search_auth_user_sec_access
 -- Name: seq_search_data_id; Type: SEQUENCE; Schema: searchapp; Owner: -
 --
 CREATE SEQUENCE seq_search_data_id
-    START WITH 978094
+    START WITH 978095
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
