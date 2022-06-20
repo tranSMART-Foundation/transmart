158d53974b370f477121633233a7f1723e0b1a58
Peter Rice <peter.rice@i2b2transmart.org>
2022-06-20 16:04:35 +0100

    Define TRANSMARTDATA in default vars file

--- transmart-data/env/vars-ubuntu.php
+++ transmart-data/env/vars-ubuntu.php
@@ -6,6 +6,7 @@ PGPASSWORD=transmart
 
 PGSQL_BIN="/usr/bin/"
 TABLESPACES=/var/lib/postgresql/tablespaces/
+TRANSMARTDATA=<?= realpath("../"), "\n" ?>
 KETTLE_JOBS_PSQL=<?= realpath(__DIR__), "/transmart-etl/Kettle/postgres/Kettle-ETL/", "\n" ?>
 R_JOBS_PSQL=<?= realpath(__DIR__), "/transmart-etl/Kettle/postgres/R/", "\n" ?>
 KITCHEN=<?= realpath(__DIR__), "/data-integration/kitchen.sh", "\n" ?>
@@ -14,5 +15,5 @@ PATH=<?= realpath(__DIR__) ?>:$PATH
 
 export PGHOST PGPORT PGDATABASE PGUSER PGPASSWORD TABLESPACES PGSQL_BIN \
 	R_JOBS_PSQL KETTLE_JOBS KETTLE_JOBS_PSQL KETTLE_JOBS_ORA KITCHEN TSUSER_HOME ORAHOST ORAPORT \
-	ORASID ORASVC ORAUSER ORAPASSWORD ORACLE_MANAGE_TABLESPACES \
+	ORASID ORASVC ORAUSER ORAPASSWORD ORACLE_MANAGE_TABLESPACES TRANSMARTDATA \
 	ORACLE_TABLESPACES_DIR ORACLE
