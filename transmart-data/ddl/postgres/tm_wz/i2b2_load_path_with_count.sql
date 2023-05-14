07b2e21247730ccdeb1e368ca02cc0a0a74c18dc
Peter Rice <peter.rice@i2b2transmart.org>
2023-05-14 18:24:33 +0100

    standard code for primary key for tm_wz/i2b2_load_path_with_count

--- transmart-data/ddl/postgres/tm_wz/i2b2_load_path_with_count.sql
+++ transmart-data/ddl/postgres/tm_wz/i2b2_load_path_with_count.sql
@@ -3,6 +3,11 @@
 --
 CREATE TABLE i2b2_load_path_with_count (
     c_fullname character varying(700),
-    nbr_children int,
-    PRIMARY KEY (c_fullname)
+    nbr_children int
 );
+
+--
+-- Name: i2b2_load_path_with_count_pk; Type: CONSTRAINT; Schema: tm_wz; Owner: -
+--
+ALTER TABLE ONLY i2b2_load_path_with_count
+    ADD CONSTRAINT i2b2_load_path_with_count_pk PRIMARY KEY (c_fullname);
