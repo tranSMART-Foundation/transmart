8a7bc8f59429119ee8ef32a6747c005c3d2535b8
Peter Rice <peter.rice@i2b2transmart.org>
2023-05-14 18:24:09 +0100

    standard code for primary key for tm_wz/i2b2_load_path

--- transmart-data/ddl/postgres/tm_wz/i2b2_load_path.sql
+++ transmart-data/ddl/postgres/tm_wz/i2b2_load_path.sql
@@ -3,6 +3,11 @@
 --
 CREATE TABLE i2b2_load_path (
     path character varying(700),
-    record_id character(10),
-    PRIMARY KEY (path, record_id)
+    record_id character(10)
 );
+
+--
+-- Name: i2b2_load_path_pk; Type: CONSTRAINT; Schema: tm_wz; Owner: -
+--
+ALTER TABLE ONLY i2b2_load_path
+    ADD CONSTRAINT i2b2_load_path_pk PRIMARY KEY (path, record_id);
