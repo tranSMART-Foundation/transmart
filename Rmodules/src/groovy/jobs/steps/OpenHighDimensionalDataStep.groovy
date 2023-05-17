5f049baacadf3d38d1058e68cf0de4d45aa63e71
Peter Rice <peter.rice@i2b2transmart.org>
2023-05-17 14:53:11 +0100

    comment out logging messages in Rmodules OpenHighDimensionalDataStep

--- Rmodules/src/groovy/jobs/steps/OpenHighDimensionalDataStep.groovy
+++ Rmodules/src/groovy/jobs/steps/OpenHighDimensionalDataStep.groovy
@@ -1,5 +1,6 @@
 package jobs.steps
 
+import groovy.util.logging.Slf4j
 import jobs.UserParameters
 import jobs.misc.AnalysisConstraints
 import jobs.misc.Hacks
@@ -9,6 +10,7 @@ import org.transmartproject.core.dataquery.highdim.assayconstraints.AssayConstra
 import org.transmartproject.core.dataquery.highdim.dataconstraints.DataConstraint
 import org.transmartproject.core.dataquery.highdim.projections.Projection
 
+@Slf4j('logger')
 class OpenHighDimensionalDataStep implements Step {
 
     final String statusName = 'Gathering Data'
@@ -28,11 +30,13 @@ class OpenHighDimensionalDataStep implements Step {
  		for (ontologyTerm in ontologyTerms) {
                     String seriesLabel = ontologyTerm.split('\\\\')[-1]
                     List<String> keyList = ['S' + (index + 1), seriesLabel]
+//		    logger.info 'execute results[{}] = fetchSubset({},{})', keyList, resultInstanceId, ontologyTerm
                     results[keyList] = fetchSubset(resultInstanceId, ontologyTerm)
                 }
             }
         }
         catch(e) {
+//	    logger.info 'execute Exception {}', e
 	    for (it in results.values()) {
 		it.close()
 	    }
@@ -52,28 +56,35 @@ class OpenHighDimensionalDataStep implements Step {
 
     private TabularResult fetchSubset(int patientSetId, String ontologyTerm) {
 
-        List<DataConstraint> dataConstraints = analysisConstraints['dataConstraints'].
-                collect { String constraintType, values ->
-                    if (values) {
-                        dataTypeResource.createDataConstraint(values, constraintType)
-                    }
-                }.grep()
+//	logger.info 'fetchSubset patientSetId {} ontologyTerm {}', patientSetId, ontologyTerm
+
+        List<DataConstraint> dataConstraints = analysisConstraints['dataConstraints'].collect { String constraintType, values ->
+            if (values) {
+//		logger.info 'fetchSubset pdataConstraint {} -> {}', constraintType, values
+                dataTypeResource.createDataConstraint(values, constraintType)
+            }
+        }.grep()
 
         List<AssayConstraint> assayConstraints = analysisConstraints['assayConstraints'].collect { String constraintType, values ->
-                    if (values) {
-                        dataTypeResource.createAssayConstraint(values, constraintType)
-                    }
-                }.grep()
+            if (values) {
+//		logger.info 'fetchSubset passayConstraint {} -> {}', constraintType, values
+                dataTypeResource.createAssayConstraint(values, constraintType)
+            }
+        }.grep()
 
+//	logger.info 'fetchSubset add assayConstaint PATIENT_SET_CONSTRAINT rid: {}', patientSetId
         assayConstraints << dataTypeResource.createAssayConstraint(
             AssayConstraint.PATIENT_SET_CONSTRAINT,
             result_instance_id: patientSetId)
 
+//	logger.info 'fetchSubset add assayConstaint ONTOLOGY_TERM_CONSTRAINT concept_key {}', ontologyTerm
         assayConstraints << dataTypeResource.createAssayConstraint(
             AssayConstraint.ONTOLOGY_TERM_CONSTRAINT,
             concept_key: ontologyTerm)
 
         Projection projection = dataTypeResource.createProjection([:], analysisConstraints['projections'][0])
+
+//	logger.info 'fetchSub calling retrieveData'
 
         dataTypeResource.retrieveData assayConstraints, dataConstraints, projection
     }
