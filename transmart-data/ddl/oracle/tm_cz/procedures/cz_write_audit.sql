3eebd7e5d418c1da0599b09451885cbcc23ba80e
Peter Rice <peter.rice@i2b2transmart.org>
2023-06-29 12:16:56 +0100

    debugging added to oracle ETL logging

--- transmart-data/ddl/oracle/tm_cz/procedures/cz_write_audit.sql
+++ transmart-data/ddl/oracle/tm_cz/procedures/cz_write_audit.sql
@@ -14,13 +14,37 @@ CREATE OR REPLACE PROCEDURE TM_CZ.CZ_WRITE_AUDIT (
 AS
 
     lastTime timestamp;
+    elapsedSecs number;
+    debugValue varchar2(255);
 
 BEGIN
+
+    begin
+    select paramvalue
+      into debugValue
+      from tm_cz.etl_settings
+     where paramname in ('debug','DEBUG');
+     exception
+     when NO_DATA_FOUND then
+     	  debugValue := 'no';
+    end;
+
+    dbms_output.put_line('Anybody there?');
+    if (coalesce(lower(debugValue),'no') != 'no') then
+	dbms_output.put_line('debugValue TRUE: '||debugValue);
+    else
+	dbms_output.put_line('debugValue FALSE: '||debugValue);
+    end if;
 
     select max(job_date)
       into lastTime
       from tm_cz.cz_job_audit
      where job_id = jobID;
+
+    elapsedSecs := COALESCE(((EXTRACT (DAY    FROM (SYSTIMESTAMP - lastTime))*24 +
+			      EXTRACT (HOUR   FROM (SYSTIMESTAMP - lastTime)))*60 +
+			      EXTRACT (MINUTE FROM (SYSTIMESTAMP - lastTime)))*60 +
+			      EXTRACT (SECOND FROM (SYSTIMESTAMP - lastTime)), 0);
 
     insert into tm_cz.cz_job_audit(
 	job_id
@@ -50,7 +74,14 @@ BEGIN
 	    ,0)
       from dual;
 
-  COMMIT;
+    if (coalesce(lower(debugValue),'no') != 'no') then
+	dbms_output.put_line('CZ_WRITE_AUDIT job:' || jobId ||
+			     ' function:' || databaseName || '.' || procedureName || ' step:' || stepNumber ||
+			     ' records:' || recordsManipulated || '  status:' || stepStatus ||
+			     '  date:' || SYSTIMESTAMP || '  elapsed:' || elapsedSecs || '        ' || stepDesc);
+    end if;
+
+    COMMIT;
 
 END;
 /
