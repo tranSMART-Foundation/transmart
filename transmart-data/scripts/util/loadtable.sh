4214ca0e4e45f7ab88f3918e9f30e62713814419
Peter Rice <peter.rice@i2b2transmart.org>
2023-08-27 16:47:39 +0100

    multi-line code for loadtable script

--- transmart-data/scripts/util/loadtable.sh
+++ transmart-data/scripts/util/loadtable.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -e
 
 # loadtable.sh <schema> <table>
-# finds the .tsv file for th etabel
-# (saves with savetable.sh)
+# finds the .tsv file for the table
+# (saved with savetable.sh)
 #
 # loads data into table
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ `psql -c "SELECT EXISTS (SELECT * FROM $1.$2 LIMIT 1)" -tA` = 't' ]; then \
-		echo "WARNING: The table $1.$2 already has data; skipped" >&2; \
-	else \
-		echo "Loading $1.$2"; \
-		psql -c "COPY $1.$2 FROM STDIN \
-			CSV DELIMITER E'\t'" < $SCRIPT_DIR/$2.tsv; \
-	fi
+if [ `psql -c "SELECT EXISTS (SELECT * FROM $1.$2 LIMIT 1)" -tA` = 't' ]; then
+    echo "WARNING: The table $1.$2 already has data; skipped" >&2;
+else
+    echo "Loading $1.$2";
+    psql -c "COPY $1.$2 FROM STDIN CSV DELIMITER E'\t'" < $SCRIPT_DIR/$2.tsv;
+fi
 
