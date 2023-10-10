19742b59430ce2a90d35e99f6b637e2b7c3f0b1c
dert1129 <nathancreger1111@gmail.com>
2023-10-10 14:36:27 -0400

    remove data Attestation configs

--- transmartApp/grails-app/services/org/transmartfoundation/config/ConfigService.groovy
+++ transmartApp/grails-app/services/org/transmartfoundation/config/ConfigService.groovy
@@ -1482,7 +1482,6 @@ class ConfigService {
             else if(k == 'ui.tabs.sampleExplorer.hide'){addConfig(uiParams,k,v,'')}
             else if(k == 'ui.tabs.search.show'){addConfig(uiParams,k,v,'')}
             else if(k == 'ui.tabs.uploadData.hide'){addConfig(uiParams,k,v,'')}
-            else if(k == 'ui.tabs.dataAttestation.hide'){addConfig(uiParams,k,v,'')}
 
             // upload
             else if(k == 'com.recomdata.dataUpload.appTitle'){addConfig(uploadParams,k,v,'')}
