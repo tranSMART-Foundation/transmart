897b024004b9a4d17efcde75e54aaa2b3a89533e
Peter Rice <peter.rice@i2b2transmart.org>
2023-11-22 15:31:17 +0000

    Install creates transmartimages directory

--- Scripts/install/InstallConfig.sh
+++ Scripts/install/InstallConfig.sh
@@ -12,9 +12,11 @@ case $TMINSTALL_OS in
     ubuntu)
 	case $TMINSTALL_OSVERSION in
 	    18.04 | 18)
+		tomcatdir="tomcat8"
 		tomcatuser="tomcat8"
 		;;
 	    20.04 | 20 | 22.04 | 22)
+		tomcatdir="tomcat9"
 		tomcatuser="tomcat"
 		;;
 	esac
@@ -31,6 +33,9 @@ make -C config install
 sudo mkdir -p /var/lib/$tomcatuser/.grails/transmartConfig/
 sudo cp $HOME/.grails/transmartConfig/*.groovy /var/lib/$tomcatuser/.grails/transmartConfig/
 sudo chown -R $tomcatuser:$tomcatuser /var/lib/$tomcatuser/.grails
+
+sudo mkdir -p /var/lib/$tomcatdir/webapps/transmartimages
+sudo chown -R $tomcatuser:$tomcatuser /var/lib/$tomcatdir/webapps/transmartimages
 
 cd $TMSCRIPTS_BASE/checks
 ./checkFilesConfig.sh
